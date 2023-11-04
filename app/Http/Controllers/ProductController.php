bd923f6ad719f54dd54c809f79537a82813efcd8
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-04 20:12:19 +0100

    Add edit, remove and update create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\ProductImage;
+use Illuminate\Support\Facades\Storage;
+
 
 class ProductController extends Controller
 {
@@ -38,17 +40,92 @@ class ProductController extends Controller
         ]);
 
         $product = Product::create($validatedData);
+        $uploadedImages = [];
         if ($request->hasFile('images')) {
             foreach ($request->file('images') as $imageFile) {
                 $imagePath = $imageFile->store('product_images', 'public');
 
-                ProductImage::create([
+                $productImage = ProductImage::create([
                     'product_id' => $product->id,
                     'image_path' => $imagePath,
                     'alt_text' => $validatedData['description'],
                 ]);
+
+                $uploadedImages[] = [
+                    'path' => $imagePath,
+                    'url'  => asset('storage/' . $imagePath),
+                ];
             }
         }
-        return redirect()->route("products.show", $product);
+        return response()->json([
+            'message' => 'Product created successfully.',
+            'product_id' => $product->id,
+            'uploaded_images' => $uploadedImages,
+        ]);
+    }
+
+    public function edit(Product $product)
+    {
+        if (auth()->id() !== $product->artisan_id) {
+            abort(403);
+        }
+
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        if (auth()->id() !== $product->artisan_id) {
+            abort(403);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric|between:0,999999.99',
+            'images' => 'sometimes|array|max:3',
+            'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        // Update product details
+        $product->update($validatedData);
+
+        // Handle image uploads
+        if ($request->hasFile('images')) {
+            $currentImageCount = $product->images()->count();
+            $allowedNewImages = 3 - $currentImageCount;
+
+            if ($allowedNewImages > 0) {
+                $images = array_slice($request->file('images'), 0, $allowedNewImages);
+
+                foreach ($images as $imageFile) {
+                    $imagePath = $imageFile->store('product_images', 'public');
+
+                    ProductImage::create([
+                        'product_id' => $product->id,
+                        'image_path' => $imagePath,
+                        'alt_text' => $product->name,
+                    ]);
+                }
+            }
+        }
+
+        return redirect()->route('products.show', $product->id)->with('success', 'Product updated successfully.');
+    }
+
+    public function destroyImage(Product $product, ProductImage $productImage)
+    {
+        if ($productImage->product_id !== $product->id) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        Storage::delete($productImage->image_path);
+
+        $productImage->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Image deleted successfully.'
+        ]);
     }
 }
