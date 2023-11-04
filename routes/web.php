ea837a70499ab42d040d95d1006e1c03c84b431b
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-04 20:13:15 +0100

    Reorganize routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\FaqController;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,28 +17,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Public Routes
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get("/products", [ProductController::class, "index"])->name("products.index");
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::get("products/{product}", [ProductController::class, "show"])->name("products.show");
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
 Route::get('/faq', [FaqController::class, 'index'])->name('faq.index');
 
+// Authenticated User Dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Auth Routes (requires authentication)
 Route::middleware('auth')->group(function () {
+    // Profile Routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Product Management Routes
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
     Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}/images/{productImage}', [ProductController::class, 'destroyImage'])->name('products.images.destroy');
 });
 
+// Authentication Routes (handled by Laravel Breeze, Jetstream, etc.)
 require __DIR__ . '/auth.php';
