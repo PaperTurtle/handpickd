33f57b16c061fde33351c53e4c8b6dc990d6b15a
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-04 20:13:06 +0100

    Add edit option to product show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -17,5 +17,9 @@
                     style="width: 100px; height: auto;">
             @endforeach
         </div>
+        @if (auth()->id() === $product->artisan_id)
+            <a class="bg-secondary rounded-full py-1 px-4" href="{{ route('products.edit', $product->id) }}"
+                class="btn btn-primary">Edit</a>
+        @endif
     </div>
 </x-app-layout>
