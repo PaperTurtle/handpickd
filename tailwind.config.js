7ef31419876087f568b4ac7f0f156627420055f0
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-09 19:04:48 +0100

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
-
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -14,6 +13,30 @@ export default {
 
 	theme: {
 		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			black: colors.black,
+			blue: colors.blue,
+			cyan: colors.cyan,
+			emerald: colors.emerald,
+			fuchsia: colors.fuchsia,
+			slate: colors.slate,
+			gray: colors.gray,
+			neutral: colors.neutral,
+			stone: colors.stone,
+			green: colors.green,
+			indigo: colors.indigo,
+			lime: colors.lime,
+			orange: colors.orange,
+			pink: colors.pink,
+			purple: colors.purple,
+			red: colors.red,
+			rose: colors.rose,
+			sky: colors.sky,
+			teal: colors.teal,
+			violet: colors.violet,
+			yellow: colors.amber,
+			white: colors.white,
 			text: "var(--text)",
 			background: "var(--background)",
 			primary: "var(--primary)",
