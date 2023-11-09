db083b1a397bcebadf7ccb4e7e0ef1cd8e9ddaf5
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-09 19:07:18 +0100

    Add redirect after successful transaction

---
+++ resources/views/checkout/success.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="container">
+        <h1>Checkout Completed</h1>
+        <p>Thank you for your purchase! Your order is being processed.</p>
+        <a href="{{ route('home') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Return Home
+        </a>
+    </div>
+</x-app-layout>
