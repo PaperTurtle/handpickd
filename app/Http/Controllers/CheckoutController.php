b9d5e6da14d2ff6af0f00a7f37f1045e48910bc3
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-11 21:44:41 +0100

    Add small documentation

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,8 +8,16 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Transaction;
 use Exception;
 
+/**
+ * CheckoutController handles operations related to the shopping cart and checkout process.
+ */
 class CheckoutController extends Controller
 {
+    /**
+     * Display the checkout page with items in the authenticated user's shopping cart.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
     public function index()
     {
         $cartItems = ShoppingCart::with('product')->where('user_id', auth()->id())->get();
@@ -17,6 +25,12 @@ class CheckoutController extends Controller
         return view('checkout.index', compact('cartItems'));
     }
 
+    /**
+     * Add a product to the authenticated user's shopping cart. If the user is not authenticated, redirect to login.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function addToCart(Request $request)
     {
         if (auth()->guest()) {
@@ -31,6 +45,12 @@ class CheckoutController extends Controller
         return redirect()->back()->with('success', 'Product added to cart!');
     }
 
+    /**
+     * Remove an item from the shopping cart by its item ID.
+     *
+     * @param  int  $cartItemId
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function removeFromCart($cartItemId)
     {
         ShoppingCart::where('id', $cartItemId)->delete();
@@ -38,6 +58,11 @@ class CheckoutController extends Controller
         return redirect()->back()->with('success', 'Product removed from cart!');
     }
 
+    /**
+     * Display the shopping cart contents for the authenticated user.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
     public function viewCart()
     {
         $cartItems = ShoppingCart::where('user_id', auth()->id())->get();
@@ -45,6 +70,14 @@ class CheckoutController extends Controller
         return view('cart.index', compact('cartItems'));
     }
 
+    /**
+     * Process the checkout by creating transactions for each item in the cart and updating product quantities.
+     * On success, the user's cart is cleared and the user is redirected to a success route.
+     * On failure, the transaction is rolled back and the user is redirected back with an error message.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function processCheckout(Request $request)
     {
         DB::beginTransaction();
@@ -74,6 +107,12 @@ class CheckoutController extends Controller
         }
     }
 
+    /**
+     * Remove an item from the shopping cart via an AJAX request. Responds with JSON.
+     *
+     * @param  int  $cartItemId
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function remove($cartItemId)
     {
         $cartItem = ShoppingCart::find($cartItemId);
