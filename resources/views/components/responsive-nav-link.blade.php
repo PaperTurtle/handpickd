0db93e79846a61346dc912335b981bccabb98907
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-11 21:46:47 +0100

    Add small styling

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -1,8 +1,8 @@
 @props(['active'])
 
 @php
-    $activeClasses = 'block w-2/3 pl-3 pr-4 py-2 font-bold text-left text-base font-medium text-blue-700 transition ease-in-out duration-150 bg-background rounded-r-full';
-    $inactiveClasses = 'block w-2/3 pl-3 pr-4 py-2 font-bold border-transparent text-left text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 transition ease-in-out duration-150 text-background hover:bg-secondary hover:text-text rounded-r-full';
+    $activeClasses = 'block w-2/3 pl-3 pr-4 py-2 font-bold text-left text-base font-medium text-text transition ease-in-out duration-150 bg-background rounded-r-full';
+    $inactiveClasses = 'block w-2/3 pl-3 pr-4 py-2 font-bold border-transparent text-left text-base font-medium text-background hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 transition ease-in-out duration-150 text-background hover:bg-secondary hover:text-text rounded-r-full';
 
     $classes = $active ? $activeClasses : $inactiveClasses;
 @endphp
