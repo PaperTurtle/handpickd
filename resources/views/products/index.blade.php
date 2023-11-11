af6208cf2c12c89912d6e7fb0dd7c7de2d1be0b2
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-11 21:47:40 +0100

    Add styling to products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,24 @@
 <x-app-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Products') }}
-        </h2>
-    </x-slot> --}}
-    <div class="container">
-        <h1 class="text-xl font-bold font-heading">Product Listings</h1>
-        <ul>
-            @foreach ($products as $product)
-                <li>
-                    <a href="{{ route('products.show', $product) }}">{{ $product->name }}</a>
-                </li>
-            @endforeach
-        </ul>
+    <div>
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 class="sr-only">Products</h2>
+
+            <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                @foreach ($products as $product)
+                    <a href="{{ route('products.show', $product->id) }}" class="group bg-secondary p-2 rounded-lg">
+                        <div
+                            class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                            @if ($product->images->first())
+                                <img src="{{ Storage::url($product->images->first()->image_path) }}"
+                                    alt="{{ $product->images->first()->alt_text }}"
+                                    class="h-full w-full object-cover object-center group-hover:opacity-75">
+                            @endif
+                        </div>
+                        <h3 class="mt-4 text-sm text-gray-700">{{ $product->name }}</h3>
+                        <p class="mt-1 text-lg font-medium text-gray-900">${{ number_format($product->price, 2) }}</p>
+                    </a>
+                @endforeach
+            </div>
+        </div>
     </div>
 </x-app-layout>
