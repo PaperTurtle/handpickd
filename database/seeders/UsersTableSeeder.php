60364cbe39df19fcec598af07d0ea86c996c464d
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-12 12:08:56 +0100

    Add more Users seed

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -11,11 +11,19 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        // Creating a sample user
-        User::create([
-            'name' => 'John Doe',
-            'email' => 'john@example.com',
-            'password' => Hash::make('password'), // Never store plain text passwords
-        ]);
+        $firstNames = ['Alice', 'Bob', 'Charlie', 'Diana', 'Ethan', 'Fiona', 'George', 'Hannah', 'Ian', 'Julia', 'Kevin', 'Luna', 'Max', 'Nora', 'Oscar', 'Penny', 'Quinn', 'Ryan', 'Sophia', 'Tyler'];
+        $lastNames = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis', 'Rodriguez', 'Martinez', 'Hernandez', 'Lopez', 'Gonzalez', 'Wilson', 'Anderson', 'Thomas', 'Taylor', 'Moore', 'Jackson', 'Martin'];
+
+        for ($i = 0; $i < 20; $i++) {
+            $firstName = $firstNames[$i];
+            $lastName = $lastNames[$i];
+            $email = strtolower($firstName) . '.' . strtolower($lastName) . '@example.com';
+
+            User::create([
+                'name' => $firstName . ' ' . $lastName,
+                'email' => $email,
+                'password' => Hash::make('password'),
+            ]);
+        }
     }
 }
