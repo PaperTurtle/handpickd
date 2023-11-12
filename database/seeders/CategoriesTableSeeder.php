8d89fff33feefded1ab225686e53ad44cc0285c5
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-12 12:08:24 +0100

    Add seed categories

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -10,10 +10,24 @@ class CategoriesTableSeeder extends Seeder
 {
     public function run()
     {
-        // Creating a sample category
-        Category::create([
-            'name' => 'Handmade Crafts',
-            'description' => 'Beautifully crafted handmade items.'
-        ]);
+        $categories = [
+            ['name' => 'Handmade Crafts', 'description' => 'Beautifully crafted handmade items.'],
+            ['name' => 'Jewelry & Accessories', 'description' => 'Handcrafted jewelry and accessories.'],
+            ['name' => 'Home & Living', 'description' => 'Handmade home decor and living products.'],
+            ['name' => 'Art & Collectibles', 'description' => 'Unique art pieces and collectibles.'],
+            ['name' => 'Clothing & Shoes', 'description' => 'Handmade clothing and shoes for all ages.'],
+            ['name' => 'Wedding & Party', 'description' => 'Personalized items for weddings and parties.'],
+            ['name' => 'Toys & Entertainment', 'description' => 'Handcrafted toys and entertainment products.'],
+            ['name' => 'Vintage', 'description' => 'Vintage items and retro finds.'],
+            ['name' => 'Bath & Beauty', 'description' => 'Handmade beauty and bath products.'],
+        ];
+
+        // Create each category in the database
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category['name'],
+                'description' => $category['description']
+            ]);
+        }
     }
 }
