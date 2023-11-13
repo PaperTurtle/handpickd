e2a06498f2d302379eebeb9b519fcd571d49075a
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-13 08:20:32 +0100

    Change to use resized images instead of originals

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,12 +6,12 @@
             <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 @foreach ($products as $product)
                     <a href="{{ route('products.show', $product->id) }}" class="group bg-secondary p-2 rounded-lg">
-                        <div
-                            class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7">
                             @if ($product->images->first())
-                                <img src="{{ Storage::url($product->images->first()->image_path) }}"
+                                <img src="{{ Storage::url($product->images->first()->resized_image_path) }}"
                                     alt="{{ $product->images->first()->alt_text }}"
-                                    class="h-48 w-full object-cover object-center group-hover:opacity-75">
+                                    class="h-[9em] w-full object-cover object-center group-hover:opacity-75"
+                                    loading="lazy" height="144" width="240">
                             @endif
                         </div>
                         <h3 class="mt-4 text-sm text-gray-700">{{ $product->name }}</h3>
