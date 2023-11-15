79fc752980e36f4ec1701a2808548512389eaf77
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-15 15:15:50 +0100

    Add functionality to edit review

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -38,13 +38,46 @@
                     <p class="text-gray-800 my-2" x-text="review.review"></p>
                     <p class="text-sm text-gray-500">Reviewed by <span x-text="review.user.name"></span> on <span
                             x-text="formatDate(review.created_at)"></span></p>
+                    <template x-if="review.user.id === {{ auth()->id() }}">
+                        <button @click="beginEditReview(review)"
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+                            Edit Review
+                        </button>
+                    </template>
                 </div>
             </template>
             <p x-show="reviews.length === 0" class="text-gray-600">No reviews yet.</p>
+            <!-- Edit Review Form -->
+            <template x-if="editingReview">
+                <div class="edit-review mt-6 w-1/3">
+                    <h2 class="text-2xl font-semibold mb-3">Edit Your Review</h2>
+                    <form @submit.prevent="updateReview()">
+                        @csrf
+                        <div>
+                            <label for="editRating" class="block text-sm font-medium text-gray-700">Rating</label>
+                            <select id="editRating" x-model="editRating"
+                                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                @for ($i = 1; $i <= 5; $i++)
+                                    <option value="{{ $i }}">{{ $i }}</option>
+                                @endfor
+                            </select>
+                        </div>
+                        <div class="mt-4">
+                            <label for="editReviewText" class="block text-sm font-medium text-gray-700">Review</label>
+                            <textarea id="editReviewText" x-model="editReviewText" rows="4"
+                                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
+                        </div>
+                        <button type="submit"
+                            class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Update Review
+                        </button>
+                    </form>
+                </div>
+            </template>
         </div>
         @if (auth()->check() && !$product->hasUserReviewed(auth()->id()) && auth()->id() !== $product->artisan_id)
             <template x-if="!userHasReviewed">
-                <div x-data="reviewForm()" @submit.prevent="submitReview" class="add-review mt-6 w-1/3">
+                <div @submit.prevent="submitReview" class="add-review mt-6 w-1/3">
                     <h2 class="text-2xl font-semibold mb-3">Write a Review</h2>
                     <form>
                         @csrf
@@ -81,6 +114,18 @@
                 review: '',
                 reviews: @json($product->reviews->load('user')),
                 userHasReviewed: {{ $product->hasUserReviewed(auth()->id()) ? 'true' : 'false' }},
+                editingReview: false,
+                editRating: 1,
+                editReviewText: '',
+                editReviewId: null,
+
+                beginEditReview(review) {
+                    this.editingReview = true;
+                    this.editRating = review.rating;
+                    this.editReviewText = review.review;
+                    this.editReviewId = review.id;
+                },
+
                 formatDate(dateString) {
                     const options = {
                         year: 'numeric',
@@ -89,6 +134,7 @@
                     };
                     return new Date(dateString).toLocaleDateString(undefined, options);
                 },
+
                 submitReview() {
                     const payload = {
                         product_id: this.productId,
@@ -115,8 +161,54 @@
                         .catch((error) => {
                             console.error('Error:', error);
                         });
-                }
+                },
 
+                updateReview() {
+                    const payload = {
+                        rating: parseInt(this.editRating, 10),
+                        review: this.editReviewText,
+                        _token: '{{ csrf_token() }}'
+                    };
+
+                    fetch('/reviews/' + this.editReviewId, {
+                            method: 'PATCH',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json',
+                                'X-CSRF-TOKEN': payload._token,
+                            },
+                            body: JSON.stringify(payload)
+                        })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('Network response was not ok');
+                            }
+                            return response.json();
+                        })
+                        .then(data => {
+                            // Find the review index using the review ID
+                            const index = this.reviews.findIndex(review => review.id === this.editReviewId);
+
+                            // If the review is found in the array
+                            if (index !== -1) {
+                                // Merge the updated review data with the existing user data
+                                this.reviews[index] = {
+                                    ...this.reviews[index],
+                                    ...data,
+                                    user: this.reviews[index].user
+                                };
+
+                                // Reset the form state
+                                this.editingReview = false;
+                                this.editReviewId = null;
+                                this.editRating = '1';
+                                this.editReviewText = '';
+                            }
+                        })
+                        .catch((error) => {
+                            console.error('Error:', error);
+                        });
+                },
             };
         }
     </script>
