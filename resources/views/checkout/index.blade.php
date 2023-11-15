fca43fe9f7f48f67f8871d39eeb4c7699b8dc74c
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-15 16:28:38 +0100

    Change formatting to int

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -7,9 +7,14 @@
                 <template x-for="cartItem in cartItems" :key="cartItem.id">
                     <div class="cart-item">
                         <p x-text="`Product Name: ${cartItem.product.name}`"></p>
-                        <p x-text="`Quantity: ${cartItem.quantity}`"></p>
-                        <p x-text="`Price: $${cartItem.product.price.toFixed(2)}`"></p>
-                        <p x-text="`Total Price: $${(cartItem.product.price * cartItem.quantity).toFixed(2)}`"></p>
+                        <p>
+                            <label for="quantity">Quantity:</label>
+                            <input type="number" id="quantity" x-model="cartItem.quantity" min="1"
+                                @change="updateCart(cartItem.id, cartItem.quantity)">
+                        </p>
+                        <p x-text="`Price: $${Number(cartItem.product.price).toFixed(2)}`"></p>
+                        <p x-text="`Total Price: $${(Number(cartItem.product.price) * cartItem.quantity).toFixed(2)}`">
+                        </p>
                         <button type="button" @click="removeFromCart(cartItem.id)"
                             class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                             Remove
@@ -19,11 +24,11 @@
             </div>
         </template>
 
-        <template x-cloak x-show="false" x-if="cartItems.length === 0">
+        <template x-cloak x-if="cartItems.length === 0">
             <p>Your shopping cart is empty.</p>
         </template>
 
-        <form action="{{ route('checkout.process') }}" method="POST" x-show="cartItems.length > 0">
+        <form action="{{ route('checkout.process') }}" method="POST" x-show="cartItems.length > 0" x-cloak>
             @csrf
             <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                 Complete Purchase
@@ -41,8 +46,7 @@
                             method: 'DELETE',
                             headers: {
                                 'Content-Type': 'application/json',
-                                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute(
-                                    'content'),
+                                'X-CSRF-TOKEN': '{{ csrf_token() }}',
                             },
                         })
                         .then(response => {
@@ -56,6 +60,28 @@
                         })
                         .catch(error => {
                             console.error('There has been a problem with your fetch operation:', error);
+                        });
+                },
+
+                updateCart(itemId, quantity) {
+                    fetch(`/cart/update/${itemId}`, {
+                            method: 'PATCH',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                            },
+                            body: JSON.stringify({
+                                quantity: quantity
+                            })
+                        })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('Network response was not ok');
+                            }
+                            return response.json();
+                        })
+                        .catch(error => {
+                            console.error('There has been a problem with your update operation:', error);
                         });
                 },
             };
