5cf6321100cc99cba2bd593f66ca929a32151d8f
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-16 19:59:37 +0100

    Formatting

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -50,7 +50,7 @@
         @endif
 
         <form method="POST" action="{{ route('products.update', $product->id) }}" enctype="multipart/form-data"
-              class="mt-6 space-y-6">
+            class="mt-6 space-y-6">
             @csrf
             @method('PUT')
 
@@ -58,14 +58,14 @@
             <div>
                 <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name:</label>
                 <input type="text" id="name" name="name" value="{{ $product->name }}" required
-                       class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </div>
 
             <!-- Category Field -->
             <div>
                 <label for="category_id" class="block text-sm font-medium leading-6 text-gray-900">Category:</label>
                 <select name="category_id" id="category_id" required
-                        class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                     @foreach (Category::all() as $category)
                         <option value="{{ $category->id }}"
                             {{ $product->category_id == $category->id ? 'selected' : '' }}>
@@ -79,15 +79,15 @@
             <div>
                 <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description:</label>
                 <textarea id="description" name="description" required
-                          class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ $product->description }}</textarea>
+                    class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ $product->description }}</textarea>
             </div>
 
             <!-- Price Field -->
             <div>
                 <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price:</label>
                 <input type="number" id="price" name="price" value="{{ $product->price }}" step="0.01"
-                       required
-                       class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    required
+                    class="mt-1 block w-1/3 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </div>
             <!-- Existing Images Field -->
             <div>
@@ -96,9 +96,9 @@
                     @foreach ($product->images as $image)
                         <div x-ref="image{{ $image->id }}" class="relative">
                             <img src="{{ Storage::url($image->image_path) }}" alt="{{ $image->alt_text }}"
-                                 class="rounded-md h-24 object-cover">
+                                class="rounded-md h-24 object-cover">
                             <button type="button" @click="removeImage({{ $image->id }})"
-                                    class="absolute top-0 right-0 mt-1 mr-1 rounded-md bg-red-500 px-2 py-1 text-sm text-white">
+                                class="absolute top-0 right-0 mt-1 mr-1 rounded-md bg-red-500 px-2 py-1 text-sm text-white">
                                 Delete
                             </button>
                         </div>
@@ -113,19 +113,18 @@
                     class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 w-1/3 bg-white">
                     <div class="text-center">
                         <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor"
-                             aria-hidden="true">
+                            aria-hidden="true">
                             <path fill-rule="evenodd"
-                                  d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                                  clip-rule="evenodd"/>
+                                d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                                clip-rule="evenodd" />
                         </svg>
                         <div class="mt-4 flex text-sm leading-6 text-gray-600">
                             <label for="images"
-                                   class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                                 <span>Upload files</span>
                                 <input id="images" name="images[]" type="file" multiple class="sr-only"
-                                       accept="image/png, image/jpeg, image/jpg, image/gif, image/svg+xml"
-                                       x-ref="images"
-                                       @change="newImages = [];
+                                    accept="image/png, image/jpeg, image/jpg, image/gif, image/svg+xml" x-ref="images"
+                                    @change="newImages = [];
                                     let selectedFiles = Array.from($refs.images.files);
                                     if (selectedFiles.length + imageCount > maxImageCount) {
                                         $refs.images.value = '';
@@ -147,14 +146,15 @@
                     </div>
                 </div>
                 @error('images')
-                <div class="text-sm text-red-600">{{ $message }}</div>
+                    <div class="text-sm text-red-600">{{ $message }}</div>
                 @enderror
                 @error('images.*')
-                <div class="text-sm text-red-600">{{ $message }}</div>
+                    <div class="text-sm text-red-600">{{ $message }}</div>
                 @enderror
                 <div id="image-preview-container" x-show="newImages.length > 0" class="mt-4 flex space-x-4">
                     <template x-for="newImage in newImages" :key="newImage">
-                        <img :src="newImage" class="h-24 rounded-md object-cover" alt="New image preview">
+                        <img :src="newImage" class="h-[15.928em] w-[15.928em] rounded-md object-cover"
+                            alt="New image preview">
                     </template>
                 </div>
             </div>
