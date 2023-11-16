93649600a3219472f920a411ebd7acdfd645266b
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-16 15:53:26 +0100

    Add new tables and entries

--- handpickd_db(1).sql
+++ handpickd_db(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2023 at 10:25 AM
+-- Generation Time: Nov 16, 2023 at 03:02 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,7 +40,15 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Handmade Crafts', 'Beautifully crafted handmade items.', '2023-11-01 16:34:34', '2023-11-01 16:34:34');
+(1, 'Handmade Crafts', 'Beautifully crafted handmade items.', '2023-11-01 16:34:34', '2023-11-01 16:34:34'),
+(3, 'Jewelry & Accessories', 'Handcrafted jewelry and accessories.', '2023-11-12 09:40:25', '2023-11-12 09:40:25'),
+(4, 'Home & Living', 'Handmade home decor and living products.', '2023-11-12 09:40:25', '2023-11-12 09:40:25'),
+(5, 'Art & Collectibles', 'Unique art pieces and collectibles.', '2023-11-12 09:40:25', '2023-11-12 09:40:25'),
+(6, 'Clothing & Shoes', 'Handmade clothing and shoes for all ages.', '2023-11-12 09:40:25', '2023-11-12 09:40:25'),
+(7, 'Wedding & Party', 'Personalized items for weddings and parties.', '2023-11-12 09:40:25', '2023-11-12 09:40:25'),
+(8, 'Toys & Entertainment', 'Handcrafted toys and entertainment products.', '2023-11-12 09:40:25', '2023-11-12 09:40:25'),
+(9, 'Vintage', 'Vintage items and retro finds.', '2023-11-12 09:40:25', '2023-11-12 09:40:25'),
+(10, 'Bath & Beauty', 'Handmade beauty and bath products.', '2023-11-12 09:40:25', '2023-11-12 09:40:25');
 
 -- --------------------------------------------------------
 
@@ -78,7 +86,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_11_12_162225_add_resized_image_path_to_product_images', 2);
 
 -- --------------------------------------------------------
 
@@ -134,8 +143,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `artisan_id`, `category_id`, `name`, `description`, `price`, `quantity`, `created_at`, `updated_at`) VALUES
-(4, 1, 1, 'Handmade Wooden Bowl', 'A beautifully carved wooden bowl that serves as a centerpiece.', 59.99, 10, '2023-11-01 16:34:34', '2023-11-01 16:34:34'),
-(6, 1, 1, 'Dna', 'A string of Dna I found on the floor', 1.99, 1, '2023-11-03 08:22:49', '2023-11-04 16:18:45');
+(4, 1, 1, 'Handmade Wooden Bowl', 'A beautifully carved wooden bowl that serves as a centerpiece.', 59.99, 6, '2023-11-01 16:34:34', '2023-11-09 16:31:53'),
+(6, 1, 1, 'Dna', 'A string of Dna I found on the floor', 1.99, 0, '2023-11-03 08:22:49', '2023-11-09 15:20:11'),
+(11, 16, 3, 'Vintage Leather Wallet', 'A durable and stylish vintage leather wallet.', 29.99, 20, '2023-11-12 09:41:12', '2023-11-12 09:41:12'),
+(12, 16, 4, 'Handwoven Scarf', 'A warm and fashionable handwoven scarf.', 34.99, 7, '2023-11-12 09:41:12', '2023-11-14 18:19:51'),
+(13, 21, 5, 'Artisanal Scented Candles', 'Soothing scented candles made with natural ingredients.', 19.99, 30, '2023-11-12 09:41:12', '2023-11-12 09:41:12'),
+(14, 21, 6, 'Hand-painted Canvas Art', 'Abstract canvas art, perfect for decorating any room.', 89.99, 5, '2023-11-12 09:41:12', '2023-11-12 09:41:12'),
+(15, 27, 7, 'Personalized Jewelry Box', 'An elegant, personalized jewelry box for your treasures.', 49.99, 12, '2023-11-12 09:41:12', '2023-11-12 09:41:12'),
+(16, 27, 8, 'Custom Engraved Pen', 'A custom engraved pen, ideal for gifts.', 24.99, 20, '2023-11-12 09:41:12', '2023-11-12 09:41:12');
 
 -- --------------------------------------------------------
 
@@ -149,15 +164,16 @@ CREATE TABLE `product_images` (
   `image_path` varchar(255) NOT NULL,
   `alt_text` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `resized_image_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `product_images`
 --
 
-INSERT INTO `product_images` (`id`, `product_id`, `image_path`, `alt_text`, `created_at`, `updated_at`) VALUES
-(14, 6, 'product_images/5Y2MbHsHMCOH0c3O4J2ILmhfXp0N0HJk75IaSEwv.jpg', 'Dna', '2023-11-07 08:05:03', '2023-11-07 08:05:03');
+INSERT INTO `product_images` (`id`, `product_id`, `image_path`, `alt_text`, `created_at`, `updated_at`, `resized_image_path`) VALUES
+(38, 6, 'product_images/product_1699861564_original.webp', 'A string of Dna I found on the floor', '2023-11-13 06:46:04', '2023-11-13 07:46:52', 'product_images/product_1699861564_resized.webp');
 
 -- --------------------------------------------------------
 
@@ -173,6 +189,49 @@ CREATE TABLE `reviews` (
   `review` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `product_id`, `user_id`, `rating`, `review`, `created_at`, `updated_at`) VALUES
+(7, 11, 18, 5, 'Exceptional quality and detail. I’m thoroughly impressed!', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(8, 11, 19, 4, 'Beautiful work, but the delivery took longer than expected.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(9, 11, 20, 3, 'Nice, but not exactly what I was expecting based on the photos.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(10, 12, 21, 5, 'Absolutely perfect! I couldn’t be happier with this purchase.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(11, 12, 22, 4, 'Great product, though the color is slightly off from the website image.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(12, 12, 23, 2, 'Arrived damaged, but customer service was helpful in resolving the issue.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(13, 13, 24, 5, 'Superb quality! It has become my favorite piece in the house.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(14, 13, 25, 4, 'Looks great, but the size was a bit smaller than I expected.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(15, 13, 26, 3, 'Decent product, but I found it a bit overpriced for the quality.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(16, 14, 27, 5, 'I am extremely pleased with this purchase. Highly recommend!', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(17, 14, 28, 4, 'Very good craftsmanship, though the packaging was not great.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(18, 14, 29, 2, 'The product is okay, but it did not meet my expectations.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(19, 15, 29, 5, 'Truly a masterpiece! The craftsmanship is top-notch.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(20, 15, 30, 4, 'Very unique and well-made, but took a while to arrive.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(21, 15, 31, 1, 'Not what I expected, and the material feels a bit cheap.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(22, 16, 30, 5, 'Stunning design and excellent quality. I highly recommend it.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(23, 16, 31, 4, 'Great product, but the packaging could be improved.', '2023-11-12 09:47:34', '2023-11-12 09:47:34'),
+(24, 12, 1, 3, 'Very interesting product', '2023-11-14 11:09:44', '2023-11-14 11:09:44'),
+(25, 11, 1, 4, 'Very cool item', '2023-11-14 11:32:28', '2023-11-14 11:32:28'),
+(26, 14, 1, 5, 'Cool Product :)', '2023-11-14 12:01:16', '2023-11-15 13:14:31'),
+(38, 13, 1, 4, 'Cool', '2023-11-14 19:55:36', '2023-11-15 14:29:15'),
+(39, 16, 1, 1, 'Rool', '2023-11-15 16:19:50', '2023-11-15 16:19:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopping_carts`
+--
+
+CREATE TABLE `shopping_carts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -191,6 +250,16 @@ CREATE TABLE `transactions` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `buyer_id`, `product_id`, `quantity`, `total_price`, `status`, `created_at`, `updated_at`) VALUES
+(1, 11, 6, 1, 1.99, 'sent', '2023-11-09 15:20:11', '2023-11-09 16:25:53'),
+(2, 11, 4, 3, 179.97, 'sent', '2023-11-09 15:20:11', '2023-11-09 16:14:03'),
+(3, 11, 4, 1, 59.99, 'sent', '2023-11-09 16:31:53', '2023-11-09 16:32:12'),
+(4, 1, 12, 3, 104.97, 'sent', '2023-11-14 18:19:51', '2023-11-14 18:20:19');
 
 -- --------------------------------------------------------
 
@@ -216,7 +285,28 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `isArtisan`) VALUES
 (1, 'Seweryn Czabanowski', 'seweryn_cz@outlook.com', NULL, '$2y$12$Lx4yjybUIz9Bfs.L/Ssfzu.evzsjd1ypIo970xrpj.US63kRCh8Qq', NULL, '2023-11-01 14:07:21', '2023-11-01 14:07:21', 1),
-(10, 'John Doe', 'john@example.com', NULL, '$2y$12$fR.GGqbu9X196vY4xfFuS.rCaHzMmVt8Z95.c81BrW8mYZbC/BbmK', NULL, '2023-11-01 16:34:34', '2023-11-01 16:34:34', 0);
+(10, 'John Doe', 'john@example.com', NULL, '$2y$12$fR.GGqbu9X196vY4xfFuS.rCaHzMmVt8Z95.c81BrW8mYZbC/BbmK', NULL, '2023-11-01 16:34:34', '2023-11-01 16:34:34', 0),
+(11, 'Some Guy', 'someguy@email.com', NULL, '$2y$12$RuNqNdgbeYT5qSPWD5HH9.88AoMYPLJFARWVo1fnkr.ofvDxaa.pi', NULL, '2023-11-09 15:02:29', '2023-11-09 15:02:29', 0),
+(12, 'Alice Smith', 'alice.smith@example.com', NULL, '$2y$12$RaeQYYWYlLw.hUrQRFQT5eS3q6sAOJupNzuVScycy7FH6dbu7fy0W', NULL, '2023-11-12 09:35:24', '2023-11-12 09:35:24', 0),
+(13, 'Bob Johnson', 'bob.johnson@example.com', NULL, '$2y$12$0QTfm9rDLmJQ9dffh7hjnuR4gbEGUrU4SSTzb7LqiHuPRJCiJKMcO', NULL, '2023-11-12 09:35:24', '2023-11-12 09:35:24', 0),
+(14, 'Charlie Williams', 'charlie.williams@example.com', NULL, '$2y$12$5Y8pFpUR0/r/1Sf/AM2/gOG/O3RGaCtbeOse5QDKLJYZ1vaRE.3OK', NULL, '2023-11-12 09:35:25', '2023-11-12 09:35:25', 0),
+(15, 'Diana Brown', 'diana.brown@example.com', NULL, '$2y$12$O8sdj8oCnVn5DH6J7BMQKeFX7Ojse6AEgzMe2eG7reDyAt8xah0Uq', NULL, '2023-11-12 09:35:26', '2023-11-12 09:35:26', 0),
+(16, 'Ethan Jones', 'ethan.jones@example.com', NULL, '$2y$12$Jptd18jPEK9UyoyBbc2EaOC.Bk0QQAIhzVZFs5k5vOhAzBXo55VgG', NULL, '2023-11-12 09:35:27', '2023-11-12 09:35:27', 1),
+(17, 'Fiona Garcia', 'fiona.garcia@example.com', NULL, '$2y$12$qRNdId.NYA/D3jS5sGHN1OMbbmUlKiRDdZZ7eSWwJhbUWYslQZSky', NULL, '2023-11-12 09:35:27', '2023-11-12 09:35:27', 0),
+(18, 'George Miller', 'george.miller@example.com', NULL, '$2y$12$4SY2cDgv2YoEq7jJpH2WUOzySFFbiUs8OHiM9bQpdg8WSkSY.SGTG', NULL, '2023-11-12 09:35:28', '2023-11-12 09:35:28', 0),
+(19, 'Hannah Davis', 'hannah.davis@example.com', NULL, '$2y$12$0OMA73Mdn58p5dSDeGhSDeEemLxvsd6J5Tx/4c/Q9BSYhS3BteKfu', NULL, '2023-11-12 09:35:28', '2023-11-12 09:35:28', 0),
+(20, 'Ian Rodriguez', 'ian.rodriguez@example.com', NULL, '$2y$12$8HpgkgvHEzXKbkDfUT4kAenz5cm0bjNOGkNpttEFGR.2Z3I//N5ly', NULL, '2023-11-12 09:35:29', '2023-11-12 09:35:29', 0),
+(21, 'Julia Martinez', 'julia.martinez@example.com', NULL, '$2y$12$UxmaUmhKogV0laLZ5qTjn.4u66RaiKbjc0KShAo7O7nUmZZf2q5h2', NULL, '2023-11-12 09:35:30', '2023-11-12 09:35:30', 1),
+(22, 'Kevin Hernandez', 'kevin.hernandez@example.com', NULL, '$2y$12$eugbvKUWnr7jCfrRZSjSmudCUVDx21dbmLr1xneeaf2R8HEZK6bhO', NULL, '2023-11-12 09:35:31', '2023-11-12 09:35:31', 0),
+(23, 'Luna Lopez', 'luna.lopez@example.com', NULL, '$2y$12$C8Txdt.ScW7nhnFgyqJvse27mdS9WPRKJmRGTS1QkW4/VSrMHi4LG', NULL, '2023-11-12 09:35:31', '2023-11-12 09:35:31', 0),
+(24, 'Max Gonzalez', 'max.gonzalez@example.com', NULL, '$2y$12$CLa5VyCeWkXgMp.VD/K7PeV1Pyoapc6BU8fe5GtMVbdtvAixeTt/a', NULL, '2023-11-12 09:35:32', '2023-11-12 09:35:32', 0),
+(25, 'Nora Wilson', 'nora.wilson@example.com', NULL, '$2y$12$Kv.o83tvLz03DZtv7hLo4Oc7htYF6PRgaW6V7UH4dU3867JHzitaC', NULL, '2023-11-12 09:35:32', '2023-11-12 09:35:32', 0),
+(26, 'Oscar Anderson', 'oscar.anderson@example.com', NULL, '$2y$12$lqkaVLPeCbtR7ny9SDUOf.grf2KCqGEP4aWYxuk6bydloNwngnKJ6', NULL, '2023-11-12 09:35:33', '2023-11-12 09:35:33', 0),
+(27, 'Penny Thomas', 'penny.thomas@example.com', NULL, '$2y$12$ycDrVzVkWO0oV7DajCLKA./DUfOBo0MjeENJPXV5gTwBInYUjjjPm', NULL, '2023-11-12 09:35:33', '2023-11-12 09:35:33', 1),
+(28, 'Quinn Taylor', 'quinn.taylor@example.com', NULL, '$2y$12$xToDSr/8WvP43l4vVPBgXelbcXHOLAlS6iLoO892i3MasXyDKivau', NULL, '2023-11-12 09:35:34', '2023-11-12 09:35:34', 0),
+(29, 'Ryan Moore', 'ryan.moore@example.com', NULL, '$2y$12$SD3DimREtQFPo6gKVT.eV.xpEboqMYobIm39Z5m2rj1ykTKH5CtSa', NULL, '2023-11-12 09:35:34', '2023-11-12 09:35:34', 0),
+(30, 'Sophia Jackson', 'sophia.jackson@example.com', NULL, '$2y$12$6fntDq24ad4dqVLH1Yv2v.tu6Ad34fYra0R/jGagr6yPF1OKUvic.', NULL, '2023-11-12 09:35:35', '2023-11-12 09:35:35', 0),
+(31, 'Tyler Martin', 'tyler.martin@example.com', NULL, '$2y$12$ayy0qBTOtfn5b.kpeEZvT.rm73nHdPns1Kk2z7nAUmuuGQJ8o5.Ou', NULL, '2023-11-12 09:35:35', '2023-11-12 09:35:35', 0);
 
 -- --------------------------------------------------------
 
@@ -243,7 +333,8 @@ CREATE TABLE `user_profiles` (
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -296,6 +387,14 @@ ALTER TABLE `reviews`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `shopping_carts`
+--
+ALTER TABLE `shopping_carts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
 -- Indexes for table `transactions`
 --
 ALTER TABLE `transactions`
@@ -325,7 +424,7 @@ ALTER TABLE `user_profiles`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -337,7 +436,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -349,31 +448,37 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `shopping_carts`
+--
+ALTER TABLE `shopping_carts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `user_profiles`
@@ -404,6 +509,13 @@ ALTER TABLE `product_images`
 ALTER TABLE `reviews`
   ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `shopping_carts`
+--
+ALTER TABLE `shopping_carts`
+  ADD CONSTRAINT `shopping_carts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `shopping_carts_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `transactions`
