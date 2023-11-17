39a63e1607207fd7581bcd05e08b7d067a63f971
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-17 21:26:15 +0100

    Add properties and attributes

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -15,6 +15,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * - product_id (bigint, unsigned): Foreign key referencing the product this image is associated with.
  * - image_path (string): File path of the image.
  * - resized_image_path (string): File path of the resized version of the image.
+ * - show_image_path (string): File path of the resized (for show blade) version of the image
  * - alt_text (string, nullable): Alternative text for the image, used for accessibility and SEO.
  * - created_at (timestamp): Timestamp when the image was created.
  * - updated_at (timestamp): Timestamp when the image was last updated.
@@ -26,6 +27,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * - product_id: Identifier of the associated product.
  * - image_path: Path of the image file.
  * - resized_image_path: Path of the resized image file.
+ * - show_image_path: Path of the show image file.
  * - alt_text: Alternative text for the image.
  */
 class ProductImage extends Model
