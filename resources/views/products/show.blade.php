ba4576da8dac45995489a0c43ad0bf708e70fd56
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-18 20:23:02 +0100

    Formatting and auth

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -116,7 +116,8 @@
                                         d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                         clip-rule="evenodd" />
                                 </svg>
-                                <p class="ml-2 text-sm text-gray-500">In stock ({{ $product->quantity }}) and ready to
+                                <p class="ml-2 text-sm text-gray-500">In stock ({{ $product->quantity }}) and ready
+                                    to
                                     ship
                                 </p>
                             @else
@@ -141,41 +142,43 @@
                     </div>
                 </div>
                 <!-- Product form -->
-                @if ($product->quantity > 0)
-                    <div class="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
-                        <section aria-labelledby="options-heading">
-                            <h2 id="options-heading" class="sr-only">Product options</h2>
+                @if (auth()->check() && auth()->id() !== $product->artisan_id)
+                    @if ($product->quantity > 0)
+                        <div class="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+                            <section aria-labelledby="options-heading">
+                                <h2 id="options-heading" class="sr-only">Product options</h2>
 
-                            <form method="POST" @submit.prevent="addToCart">
-                                @csrf
-                                <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                <form method="POST" @submit.prevent="addToCart">
+                                    @csrf
+                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
 
-                                {{-- <div class="mt-4">
+                                    {{-- <div class="mt-4">
                                     <label for="quantity-0"
                                         class="block text-sm font-medium text-gray-700">Quantity</label>
                                     <input type="number" name="quantity" value="1" min="1"
                                         max="{{ $product->quantity }}" class="rounded border-gray-300">
                                 </div> --}}
 
-                                <div class="mt-10">
-                                    <button type="submit"
-                                        class="flex w-full items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-accent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">Add
-                                        to Cart</button>
-                                </div>
-                                <div class="mt-6 text-center">
-                                    <a href="#" class="group inline-flex text-base font-medium">
-                                        <svg class="mr-2 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                            stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
-                                        </svg>
-                                        <span class="text-gray-500 hover:text-gray-700">Lifetime Guarantee</span>
-                                    </a>
-                                </div>
-                            </form>
-                        </section>
-                    </div>
+                                    <div class="mt-10">
+                                        <button type="submit"
+                                            class="flex w-full items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-accent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">Add
+                                            to Cart</button>
+                                    </div>
+                                    <div class="mt-6 text-center">
+                                        <a href="#" class="group inline-flex text-base font-medium">
+                                            <svg class="mr-2 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                stroke="currentColor" aria-hidden="true">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+                                            </svg>
+                                            <span class="text-gray-500 hover:text-gray-700">Lifetime Guarantee</span>
+                                        </a>
+                                    </div>
+                                </form>
+                            </section>
+                        </div>
+                    @endif
                 @endif
             </div>
         </div>
@@ -347,13 +350,14 @@
                                         <p class="mt-1 text-sm text-gray-500">Updated on <span
                                                 x-text="formatDate(review.updated_at)"></span></p>
                                     </template>
-
-                                    <template x-if="review.user.id === {{ auth()->id() }}">
-                                        <button @click="beginEditReview(review)"
-                                            class="flex items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-accent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
-                                            Edit Review
-                                        </button>
-                                    </template>
+                                    @auth
+                                        <template x-if="review.user.id === {{ auth()->id() }}">
+                                            <button @click="beginEditReview(review)"
+                                                class="flex items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-accent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
+                                                Edit Review
+                                            </button>
+                                        </template>
+                                    @endauth
                                 </div>
                             </template>
                             <p x-show="reviews.length === 0" class="text-gray-600">No reviews yet.</p>
