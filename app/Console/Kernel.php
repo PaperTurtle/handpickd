e5f84e485dcdfce7ca1a769f502b1a5beca2d211
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-18 13:24:43 +0100

    Add command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,6 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected $commands = [
         \App\Console\Commands\CleanOrphanedImages::class,
+        \App\Console\Commands\ProcessProductImages::class,
     ];
     /**
      * Define the application's command schedule.
