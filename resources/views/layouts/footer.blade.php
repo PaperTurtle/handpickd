932fdfb0643b1bf0695bd6d5306daf92b66c1bb1
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-18 19:46:02 +0100

    Add margin

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="flex justify-center items-center bg-primary h-16">
+<footer class="flex justify-center items-center bg-primary h-16 mt-16">
     <p class="font-body text-background font-semibold font-poppins">
         Copyright &#169; 2023 Handpickd. All Rights Reserved
     </p>
