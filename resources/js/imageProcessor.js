337d77ead0d068b1288a4a2b65f3099ecb24c02c
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-18 17:57:28 +0100

    Resize image

--- resources/js/imageProcessor.js
+++ resources/js/imageProcessor.js
@@ -3,7 +3,7 @@ import sharp from "sharp";
 const [inputPath, outputPath] = process.argv.slice(2);
 
 sharp(inputPath)
-	.resize(255, 255)
+	.resize(384, 576)
 	.webp({ quality: 100 })
 	.toFile(outputPath)
 	.then(() => console.log("Image processed and saved successfully"))
