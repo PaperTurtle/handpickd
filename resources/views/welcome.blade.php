a99cba526a96f1dde46abc8dc62791d88b0d2892
Loran Heinzel <loran.heinzel@gmx.de>
2023-11-18 15:04:22 +0100

    Image Grid + Title for Head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,62 @@
 <x-app-layout>
-    <h1>Welcome Handpickd</h1>
+
+<div class="bg-[#FFFAEB]">
+
+
+  <header class="relative overflow-hidden">
+    
+
+    <!-- Hero section -->
+    <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+      <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+        <div class="sm:max-w-lg">
+          <h1 class="text-4xl font-bold tracking-tight text-[#355427] sm:text-6xl">Handpickd</h1>
+          <p class="mt-4 text-xl text-gray-500">Welcome to Handpickd, a community-driven platform that celebrates creativity and craftsmanship. </br> Handpickd provides a space for artisans to showcase their handmade goods and for enthusiasts to discover unique, handcrafted items.</p>
+        </div>
+        <div>
+          <div class="mt-10">
+            <!-- Decorative image grid -->
+            <div aria-hidden="true" class="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl">
+              <div class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                <div class="flex items-center space-x-6 lg:space-x-8">
+                  <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                    <div class="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                      <img src="https://tinyurl.com/ywrvgryr" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="h-64 w-44 overflow-hidden rounded-lg">
+                      <img src="https://tinyurl.com/yp7rtwer" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                  </div>
+                  <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                    <div class="h-64 w-44 overflow-hidden rounded-lg">
+                      <img src="https://tinyurl.com/ys3cgyjz" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="h-64 w-44 overflow-hidden rounded-lg">
+                      <img src="https://tinyurl.com/yoevvb6c" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="h-64 w-44 overflow-hidden rounded-lg">
+                      <img src="https://tinyurl.com/yw6thpkx" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                  </div>
+                  <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                    <div class="h-64 w-44 overflow-hidden rounded-lg">
+                      <img src="https://tinyurl.com/yk9uawgc" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="h-64 w-44 overflow-hidden rounded-lg">
+                      <img src="https://tinyurl.com/yr5cpxm6" alt="" class="h-full w-full object-cover object-center">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <a href="#" class="inline-block rounded-md border border-transparent bg-[#355427] px-8 py-3 text-center font-bold text-[#FFFAEB] hover:bg-[#DCECD5] hover:text-[#355427]">Shop Collection</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+</div>
+
 </x-app-layout>
