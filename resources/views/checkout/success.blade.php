ef772f9d49f35d372d961944f28cfd147ddea808
Tobias Neubert <tneubert36@gmail.com>
2023-11-18 12:01:51 +0100

    Test commit

--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -4,6 +4,7 @@
         <p>Thank you for your purchase! Your order is being processed.</p>
         <a href="{{ route('home') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Return Home
+            
         </a>
     </div>
 </x-app-layout>
