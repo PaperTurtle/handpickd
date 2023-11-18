ebcec9059e8f36806b4e1db9b5bf7f37a9792be8
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-18 13:24:38 +0100

    Add highest quality to processor

--- resources/js/imageProcessor.js
+++ resources/js/imageProcessor.js
@@ -4,6 +4,7 @@ const [inputPath, outputPath] = process.argv.slice(2);
 
 sharp(inputPath)
 	.resize(255, 255)
+	.webp({ quality: 100 })
 	.toFile(outputPath)
 	.then(() => console.log("Image processed and saved successfully"))
 	.catch((err) => console.error("Error processing image:", err));
