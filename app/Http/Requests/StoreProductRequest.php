a6185ecaf9eddd3d33f1cc762e6e9b52ed327154
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-19 19:22:18 +0100

    Add validation rules for Product Controller store()

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * A form request class for storing product data.
+ *
+ * This class defines the validation rules and authorization for storing product information.
+ * It is used when processing HTTP requests related to creating new products.
+ */
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'artisan_id' => 'required|exists:users,id',
+            'category_id' => 'required|exists:categories,id',
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric|between:0,999999.99',
+            'quantity' => 'required|integer|min:0',
+            'images' => 'sometimes|array|max:3',
+            'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:10240',
+        ];
+    }
+}
