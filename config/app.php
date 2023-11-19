7bfc3b7903a14aef404f48a8fd770808a9bd3cd7
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-19 19:52:47 +0100

    Removed Intervention class

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -184,7 +183,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
