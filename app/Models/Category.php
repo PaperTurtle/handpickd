f35d3909bba58e6818980499d6a3e490607d8aa7
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-20 10:49:25 +0100

    Add cast

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -41,6 +41,7 @@ class Category extends Model
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
+        'description' => 'string',
     ];
 
     /**
