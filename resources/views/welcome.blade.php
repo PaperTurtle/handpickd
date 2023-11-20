794ec62307a64e8dcc4cfc7819f6c1b1a1c89fef
Loran Heinzel <loran.heinzel@gmx.de>
2023-11-20 14:30:57 +0100

    Fertigstellung statisches Layout Kategorien

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,25 +62,27 @@
 
     <!-- Category section -->
     <section aria-labelledby="category-heading" class="bg-[#355427]">
-      <div class="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
+      <div class="mx-auto max-w-[2000px] px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
         <div class="sm:flex sm:items-baseline sm:justify-between">
           <h2 id="category-heading" class="text-2xl font-bold tracking-tight text-[#FFFAEB]">Shop by Category</h2>
-          <a href="#" class="hidden text-sm font-semibold rounded-md border border-transparent bg-[#FFFAEB] px-4 py-1 text-[#355427] hover:shadow-lg hover:text-base hover:text-[#355427] sm:block">
+          <!-- Browse Categories Desktop -->
+          <a href="#" class="hidden text-sm font-semibold rounded-md border border-transparent bg-[#FFFAEB] px-4 py-1 text-[#355427] hover:shadow-lg hover:scale-105 hover:text-[#355427] sm:block">
             Browse all categories
             <span aria-hidden="true"> &rarr;</span>
           </a>
         </div>
 
-        <div class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
-          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2 transition ease-in-out delay-150 hover:scale-105 hover:shadow">
-            <img src="https://tinyurl.com/yrprq2e8" alt="diamond drop earrings on a blue background" class="object-cover object-center">
+        <!-- Erstes Element -->
+        <div class="mt-6 grid grid-cols-1 grid-rows-2 gap-y-6 sm:grid-cols-5 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+          <div class="group aspect-h-1 aspect-w-3 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-1 transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://images.pexels.com/photos/8063850/pexels-photo-8063850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="orange clay jugs on a table" class="object-cover object-center">
             <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50"></div>
             <div class="flex items-end p-6">
               <div>
                 <h3 class="font-semibold text-white">
                   <a href="#">
                     <span class="absolute inset-0"></span>
-                    Jewelery
+                    Handmade Crafts
                   </a>
                 </h3>
                 <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
@@ -88,15 +90,16 @@
             </div>
           </div>
 
+          <!-- Zweites Element -->
           <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full transition ease-in-out delay-150 hover:scale-105 hover:shadow">
-            <img src="https://tinyurl.com/yk8csec3" alt="a brown leather camera bag and a bottle of orange juice on top of a wooden desk" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
+            <img src="https://tinyurl.com/yrprq2e8" alt="diamond drop earrings on a blue background" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
             <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
             <div class="flex items-end p-6 sm:absolute sm:inset-0">
               <div>
                 <h3 class="font-semibold text-white">
                   <a href="#">
                     <span class="absolute inset-0"></span>
-                    Accessories
+                    Jewelry & Accessories
                   </a>
                 </h3>
                 <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
@@ -104,15 +107,118 @@
             </div>
           </div>
 
-          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full transition ease-in-out delay-150 hover:scale-105 hover:shadow">
-            <img src="https://tinyurl.com/yocfe2m8" alt="a hanging rack in a industrial looking room" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
+          <!-- Drittes Element -->
+          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full sm:row-span-2 transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://images.pexels.com/photos/18946616/pexels-photo-18946616/free-photo-of-schreibtisch-pflanze-fenster-zimmer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="a hanging rack in a industrial looking room" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
             <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
             <div class="flex items-end p-6 sm:absolute sm:inset-0">
               <div>
                 <h3 class="font-semibold text-white">
                   <a href="#">
                     <span class="absolute inset-0"></span>
-                    Clothing
+                    Home & Living
+                  </a>
+                </h3>
+                <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Viertes Element -->
+          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-auto transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://images.pexels.com/photos/1579708/pexels-photo-1579708.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="wall with many different paintings" class="object-cover object-center">
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50"></div>
+            <div class="flex items-end p-6">
+              <div>
+                <h3 class="font-semibold text-white">
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Art & Collectibles
+                  </a>
+                </h3>
+                <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Fünftes Element -->
+          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://tinyurl.com/yocfe2m8" alt="a hanging rack in an industrial like room" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
+            <div class="flex items-end p-6 sm:absolute sm:inset-0">
+              <div>
+                <h3 class="font-semibold text-white">
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Clothing & Shoes
+                  </a>
+                </h3>
+                <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Sechstes Element -->
+          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://images.pexels.com/photos/313707/pexels-photo-313707.jpeg?auto=compress&cs=tinysrgb&w=600" alt="wedding shoes and bouquet of white roses" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
+            <div class="flex items-end p-6 sm:absolute sm:inset-0">
+              <div>
+                <h3 class="font-semibold text-white">
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Wedding & Party
+                  </a>
+                </h3>
+                <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Siebtes Element -->
+          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://images.pexels.com/photos/3661222/pexels-photo-3661222.jpeg?auto=compress&cs=tinysrgb&w=600" alt="a wooden giraffe, crocodile and an elephant sit on top of a book" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
+            <div class="flex items-end p-6 sm:absolute sm:inset-0">
+              <div>
+                <h3 class="font-semibold text-white">
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Toys & Entertainment
+                  </a>
+                </h3>
+                <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Achtes Element -->
+          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://images.pexels.com/photos/247929/pexels-photo-247929.jpeg?auto=compress&cs=tinysrgb&w=600" alt="a chair with a chest on top and a bike next to each other outside of a store" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
+            <div class="flex items-end p-6 sm:absolute sm:inset-0">
+              <div>
+                <h3 class="font-semibold text-white">
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Vintage
+                  </a>
+                </h3>
+                <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Neuntes Element -->
+          <div class="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full transition ease-in-out delay-150 hover:scale-105 hover:shadow">
+            <img src="https://images.pexels.com/photos/4239013/pexels-photo-4239013.jpeg?auto=compress&cs=tinysrgb&w=600" alt="a white sink surrounded by a circular mirror with cosmetics on top" class="object-cover object-center sm:absolute sm:inset-0 sm:h-full sm:w-full">
+            <div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"></div>
+            <div class="flex items-end p-6 sm:absolute sm:inset-0">
+              <div>
+                <h3 class="font-semibold text-white">
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Bath & Beauty
                   </a>
                 </h3>
                 <p aria-hidden="true" class="mt-1 text-sm text-white">Shop now</p>
@@ -120,7 +226,11 @@
             </div>
           </div>
         </div>
-    </section> 
+
+        
+        
+      </div>
+    </section>
   </main>
     
         
