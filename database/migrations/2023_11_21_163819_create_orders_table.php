d81696d9f748ad8632d14c35e662fae66273b8af
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-21 19:56:28 +0100

    Add migration

---
+++ database/migrations/2023_11_21_163819_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('order_uuid')->unique();
+            $table->foreignId('buyer_id')->constrained('users');
+            $table->decimal('total_price', 20, 2)->default(0);
+            $table->boolean('is_complete')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
