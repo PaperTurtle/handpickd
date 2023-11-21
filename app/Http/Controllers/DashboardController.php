4928782da96b9fd241181c89e5c4458fa98f4ea8
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-21 19:53:47 +0100

    TODO: Fix Auth

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -47,6 +47,7 @@ class DashboardController extends Controller
      */
     public function markAsSent(Transaction $transaction): JsonResponse
     {
+        // TODO: FIX THIS!!!!
         // if ($transaction->product->artisan_id !== Auth::id()) {
         //     return response()->json(['message' => 'Unauthorized action'], 403);
         // }
