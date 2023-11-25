cb3054800d7a560d10a7ed49546cf1057c4dbe2d
Tobias Neubert <tneubert36@gmail.com>
2023-11-25 22:50:27 +0100

    Add picture placeholders; Change cart-item to flex layout

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -9,28 +9,39 @@
                 <template x-if="cartItems.length > 0">
                     <div class="cart-items divide-y divide-gray-200 border-b border-t border-gray-200">
                         <template x-for="cartItem in cartItems" :key="cartItem.id">
-                            <div class="cart-item">
-                                <p x-text="`Product Name: ${cartItem.product.name}`"></p>
-                                <p>
-                                    <label for="quantity">Quantity:</label>
-                                    <input type="number" id="quantity" x-model="cartItem.quantity" min="1"
-                                        :max="cartItem.product.quantity"
-                                        @change="updateCart(cartItem.id, cartItem.quantity)">
-                                </p>
-                                <p x-text="`Price: $${Number(cartItem.product.price).toFixed(2)}`"></p>
-                                <p
-                                    x-text="`Total Price: $${(Number(cartItem.product.price) * cartItem.quantity).toFixed(2)}`">
-                                </p>
-                                <template x-if="cartItem.product.quantity <= 0">
-                                    <p>Ships in 3-4 weeks</p>
-                                </template>
-                                <template x-if="cartItem.product.quantity > 0">
-                                    <p>In stock</p>
-                                </template>
-                                <button type="button" @click="removeFromCart(cartItem.id)"
-                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                                    Remove
-                                </button>
+                            <div class="cart-item flex">
+                                <!-- picture -->
+                                <div class="flex-shrink-0">
+                                    <img src=""
+                                        alt=""
+                                        class="h-24 w-24 rounded-md object-cover object-center sm:h-48 sm:w-48">
+                                </div>
+
+                                <div class="flex flex-1 flex-col justify-between">
+                                    
+                                    <!-- name -->
+                                    <p x-text="`${cartItem.product.name}`"></p>
+                                    <!-- quantity -->
+                                    <p>
+                                        <label for="quantity">Quantity:</label>
+                                        <input type="number" id="quantity" x-model="cartItem.quantity" min="1"
+                                            :max="cartItem.product.quantity"
+                                            @change="updateCart(cartItem.id, cartItem.quantity)">
+                                    </p>
+                                    <!-- price -->
+                                    <p x-text="`Price: $${Number(cartItem.product.price).toFixed(2)}`"></p>
+                                    <!-- in stock / not in stock -->
+                                    <template x-if="cartItem.product.quantity <= 0">
+                                        <p>Ships in 3-4 weeks</p>
+                                    </template>
+                                    <template x-if="cartItem.product.quantity > 0">
+                                        <p>In stock</p>
+                                    </template>
+                                    <button type="button" @click="removeFromCart(cartItem.id)"
+                                        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                        Remove
+                                    </button>
+                                </div>
                             </div>
                         </template>
                     </div>
@@ -46,14 +57,13 @@
                 <template x-for="cartItem in cartItems" :key="cartItem.id">
                     <div>
                         <h3 x-text="`${cartItem.product.name}`"></h3>
-                        <p x-text="`${cartItem.product.price} €`"></p>
+                        <p x-text="`${Number(cartItem.product.price * cartItem.quantity).toFixed(2)} €`"></p>
                     </div>
                 </template>
                 <p x-text="`Total Price: $${calculateTotalPrice()} €`"></p>
 
                 <a href="{{ route('checkout.process') }}">proceed to checkout</a>
                 <form action="{{ route('checkout.process') }}" method="POST" x-show="cartItems.length > 0" x-cloak>
-                    @csrf
                     <button type="submit"
                         class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                         Complete Purchase
