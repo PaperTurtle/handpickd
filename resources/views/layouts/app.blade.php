eaa67118df46be4fbbe05a9c2371a6753c7b552c
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-25 12:11:49 +0100

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Handpickd</title>
+    <link rel="icon" type="image/x-icon" href="{{ URL('images/favicon.ico') }}" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
