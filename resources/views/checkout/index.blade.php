b38f1c24480710f24665530f3e34f70d803e0278
Tobias Neubert <tneubert36@gmail.com>
2023-11-25 23:42:20 +0100

    Add svgs to In Stock and Ships in...

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -50,10 +50,26 @@
 
                                     <!-- in stock / not in stock -->
                                     <template x-if="cartItem.product.quantity <= 0">
-                                        <p>Ships in 3-4 weeks</p>
+                                        <div class="flex">
+                                            <svg class="h-5 w-5 flex-shrink-0 text-gray-300" viewBox="0 0 20 20"
+                                                    fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd"
+                                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                                <p x-text="`Ships in 3-4 weeks`"></p>
+                                        </div>    
                                     </template>
                                     <template x-if="cartItem.product.quantity > 0">
-                                        <p>In stock</p>
+                                        <div class="flex">
+                                            <svg class="h-5 w-5 flex-shrink-0 text-accent " viewBox="0 0 20 20"
+                                                fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd"
+                                                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                            <p x-text="`In stock`"></p>
+                                        </div>
                                     </template>
                                 </div>
                             </div>
