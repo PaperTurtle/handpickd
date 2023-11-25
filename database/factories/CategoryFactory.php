5981c9586121a1446e63d59904dd39dbe5729b80
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-25 20:38:53 +0100

    Add unique to factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -15,7 +15,7 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->word,
+            'name' => $this->faker->unique()->word,
             'description' => $this->faker->sentence,
         ];
     }
