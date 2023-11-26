c7052515f436ad27fb4d3455b8a2dd6e4a3944a5
Tobias Neubert <tneubert36@gmail.com>
2023-11-26 18:04:10 +0100

    Add styles to custom dropdown

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -36,18 +36,19 @@
                                                 @click.away="closeOnClickAway">
 
                                                 <!-- Button -->
-                                                <!-- <button type="button" class="w-full rounded-md bg-white 
-                                        py-2 text-sm font-semibold text-text " id="menu-button" @click="open = !open"> -->
                                                 <button type="button"
-                                                    class="inline-flex w-full justify-center bg-white 
-                                                    py-2 text-sm text-text" @click="open = !open">
-                                                        <div class="row-span-6" x-text="cartItem.quantity"></div>
-                                                        <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-                                                            fill="currentColor" aria-hidden="true">
-                                                            <path fill-rule="evenodd"
-                                                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                                                clip-rule="evenodd" />
-                                                        </svg>
+                                                    class="inline-flex w-full justify-center bg-white gap-x-1.5 rounded-md 
+                                                    py-2 text-sm text-text font-semibold shadow-sm ring-1 
+                                                    ring-inset ring-gray-300 hover:bg-gray-50
+                                                     focus:ring-2 focus:ring-accent"
+                                                    @click="open = !open">
+                                                    <div class="row-span-6" x-text="cartItem.quantity"></div>
+                                                    <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                                                        fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd"
+                                                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                                            clip-rule="evenodd" />
+                                                    </svg>
                                                 </button>
                                                 <!-- items -->
                                                 <ul class="py-1 absolute right-0 z-10 mt-2 w-16 origin-top-right
