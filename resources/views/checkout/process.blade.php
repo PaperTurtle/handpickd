56c884a2c740677f337a68a5aa5df4d077a393c1
Tobias Neubert <tneubert36@gmail.com>
2023-11-27 12:55:17 +0100

    Add first draft checkout formular

---
+++ resources/views/checkout/process.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <div class="text-text grid flex-col">
+        <label>
+            <h2 class="text-2xl">Checkout</h2>
+        </label>
+
+        <label class="text-xl">Contact information</label>
+
+        <label class="text-md">Email address</label>
+        <input type="email" class="w-40 h-5"></input>
+        <label class="text-md">Phone number</label>
+        <input type="text" class="w-40 h-5"></input>
+
+        <label class="text-xl">Shipping information</label>
+
+        <label class="text-md">First name</label>
+        <input type="text" class="w-40 h-5"></input>
+        <label class="text-md">Last name</label>
+        <input type="text" class="w-40 h-5"></input>
+        <label class="text-md">Address</label>
+        <input type="text" class="w-40 h-5"></input>
+        <label class="text-md">City</label>
+        <input type="text" class="w-40 h-5"></input>
+        <label class="text-md">Country</label>
+        <input type="text" class="w-40 h-5"></input>
+        <label class="text-md">State / Province</label>
+        <input type="text" class="w-40 h-5"></input>
+        <label class="text-md">Postal code</label>
+        <input type="text" class="w-40 h-5"></input>
+
+        <button type="submit" class="w-40 h-10 bg-white">Confirm order</button>
+    </div>
+</x-app-layout>
