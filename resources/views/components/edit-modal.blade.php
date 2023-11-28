15c87c67e7b59c6642f18cd106e456ce54d72052
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-28 19:07:52 +0100

    Add id

--- resources/views/components/edit-modal.blade.php
+++ resources/views/components/edit-modal.blade.php
@@ -1,7 +1,7 @@
 <div x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
     x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100"
-    x-transition:leave-end="opacity-0" x-cloak x-show="editingReview" aria-labelledby="modal-title" class="relative z-10"
-    aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    x-transition:leave-end="opacity-0" x-cloak id="editReviewModal" x-show="editingReview" aria-labelledby="modal-title"
+    class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
     </div>
@@ -13,7 +13,7 @@
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100 transform translate-y-0 sm:scale-100"
             x-transition:leave-end="opacity-0 transform translate-y-4 sm:translate-y-0 sm:scale-95"
-            class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
+            class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div
                 class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div class="sm:flex sm:items-start">
