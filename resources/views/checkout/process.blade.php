31971066cb748d344c25d17d422b47b07048ab19
Tobias Neubert <tneubert36@gmail.com>
2023-11-28 10:29:11 +0100

    Update layout of checkout

--- resources/views/checkout/process.blade.php
+++ resources/views/checkout/process.blade.php
@@ -1,33 +1,54 @@
 <x-app-layout>
-    <div class="text-text grid flex-col">
-        <label>
-            <h2 class="text-2xl">Checkout</h2>
-        </label>
-
-        <label class="text-xl">Contact information</label>
-
-        <label class="text-md">Email address</label>
-        <input type="email" class="w-40 h-5"></input>
-        <label class="text-md">Phone number</label>
-        <input type="text" class="w-40 h-5"></input>
-
-        <label class="text-xl">Shipping information</label>
-
-        <label class="text-md">First name</label>
-        <input type="text" class="w-40 h-5"></input>
-        <label class="text-md">Last name</label>
-        <input type="text" class="w-40 h-5"></input>
-        <label class="text-md">Address</label>
-        <input type="text" class="w-40 h-5"></input>
-        <label class="text-md">City</label>
-        <input type="text" class="w-40 h-5"></input>
-        <label class="text-md">Country</label>
-        <input type="text" class="w-40 h-5"></input>
-        <label class="text-md">State / Province</label>
-        <input type="text" class="w-40 h-5"></input>
-        <label class="text-md">Postal code</label>
-        <input type="text" class="w-40 h-5"></input>
-
-        <button type="submit" class="w-40 h-10 bg-white">Confirm order</button>
+    <label>
+        <h2 class="text-2xl">Checkout</h2>
+    </label>
+    <div class="text-text lg:grid">
+        <section class="col-span-4">
+            <label class="text-xl">Contact information</label>
+            <div class="lg:grid">
+                <label class="text-md">Email address</label>
+                <input type="email" class="w-40 h-5"></input>
+            </div>
+            <div class="lg:grid">
+                <label class="text-md">Phone number</label>
+                <input type="text" class="w-40 h-5"></input>
+            </div>
+        </section>
+        <section class="col-span-8 lg:grid">
+            <label class="text-xl">Shipping information</label>
+            <div class="flex">
+                <div class="lg:grid">
+                    <label class="text-md">First name</label>
+                    <input type="text" class="w-40 h-5"></input>
+                </div>
+                <div class="lg:grid">
+                    <label class="text-md">Last name</label>
+                    <input type="text" class="w-40 h-5"></input>
+                </div>
+            </div>
+            <label for="address" class="text-md">Address</label>
+            <input if="address" type="text" class="w-40 h-5"></input>
+            <div class="flex">
+                <div class="lg:grid">
+                    <label class="text-md">City</label>
+                    <input type="text" class="w-40 h-5"></input>
+                </div>
+                <div class="lg:grid">
+                    <label class="text-md">Country</label>
+                    <input type="text" class="w-40 h-5"></input>
+                </div>
+            </div>
+            <div class="flex">
+                <div class="lg:grid">
+                    <label class="text-md">State / Province</label>
+                    <input type="text" class="w-40 h-5"></input>
+                </div>
+                <div class="lg:grid">
+                    <label class="text-md">Postal code</label>
+                    <input type="text" class="w-40 h-5"></input>
+                </div>
+            </div>
+        </section>
     </div>
+    <button type="submit" class="w-40 h-10 bg-white">Confirm order</button>
 </x-app-layout>
