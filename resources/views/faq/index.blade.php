152037c2595988c51b555ed96596356d12ff33b8
Tobias Neubert <tneubert36@gmail.com>
2023-11-28 19:22:46 +0100

    Add logic to open questions tabs

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -1,11 +1,14 @@
 <x-app-layout>
-    <div class="container" x-data="{ open: false }">
+    <div class="container" x-data="{ openGeneral: false, openForCustomers: false, openForArtisans: false }">
         <h2>FAQ / Support</h2>
         <h2>Frequently asked questions</h2>
         <div class="flex w-fit">
-            <button class="border-2 rounded-md px-3 border-accent">General</button>
-            <button class="border-2 rounded-md px-3 border-accent">For our Customers</button>
-            <button class="border-2 rounded-md px-3 border-accent">For our Artisans</button>
+            <button class="border-2 rounded-md px-3 border-accent" @click="openGeneral = !openGeneral, openForCustomers = false, openForArtisans = false">General</button>
+            <button class="border-2 rounded-md px-3 border-accent" @click="openForCustomers = !openForCustomers, openGeneral = false, openForArtisans = false">For our Customers</button>
+            <button class="border-2 rounded-md px-3 border-accent"  @click="openForArtisans = !openForArtisans, openGeneral = false, openForCustomers = false">For our Artisans</button>
         </div>
+        <div x-show="openGeneral">General questions</div>
+        <div x-show="openForCustomers">Questions for customers</div>
+        <div x-show="openForArtisans">Questions for artisans </div>
     </div>
 </x-app-layout>
