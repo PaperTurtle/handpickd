44e89916736421bba63308c7b560409ffb64bde8
Loran Heinzel <loran.heinzel@gmx.de>
2023-11-28 18:40:46 +0100

    Route::post

--- routes/web.php
+++ routes/web.php
@@ -78,8 +78,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/email/verify', function () {
         return view('auth.verify-email');
     })->middleware('auth')->name('verification.notice');
-     
     
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+        
+        return redirect ('/dashboard');
+    })->middleware(['/auth', 'signed'])->name('verification.verify');
+
+    Route::post('/email/verification-notification', function (Request $request)) {
+        $request->user()->sendEmailVerificationNotification();
+
+        
 });
 
 // ========= Authentication Routes (Laravel Breeze, Jetstream, etc.) =========
