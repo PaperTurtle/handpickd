4d4eb94769f013208b08e0333bd98494d81e64a9
Loran Heinzel <loran.heinzel@gmx.de>
2023-11-28 19:44:29 +0100

    Farbanpassung

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="bg-primary hover:bg-accent">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
