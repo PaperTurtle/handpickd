115de8b7fad63b0334ca27bb2e8812f193d5bcf5
Tobias Neubert <tneubert36@gmail.com>
2023-11-28 19:13:02 +0100

    First commit - Add general overview

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -1,6 +1,11 @@
 <x-app-layout>
-    <div class="container">
-        <h1>FAQ / Support</h1>
-        <p>Faq Text</p>
+    <div class="container" x-data="{ open: false }">
+        <h2>FAQ / Support</h2>
+        <h2>Frequently asked questions</h2>
+        <div class="flex w-fit">
+            <button class="border-2 rounded-md px-3 border-accent">General</button>
+            <button class="border-2 rounded-md px-3 border-accent">For our Customers</button>
+            <button class="border-2 rounded-md px-3 border-accent">For our Artisans</button>
+        </div>
     </div>
 </x-app-layout>
