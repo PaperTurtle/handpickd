378565b02f3605a7738ed3201345cecc6167a64c
Loran Heinzel <loran.heinzel@gmx.de>
2023-11-28 19:23:05 +0100

    Remove Email Verification Routes

--- routes/web.php
+++ routes/web.php
@@ -73,24 +73,6 @@ Route::middleware('auth')->group(function () {
 
     // Update cart item
     Route::patch('/cart/update/{itemId}', [CheckoutController::class, 'updateCart']);
-
-   // Email Verification
-    Route::get('/email/verify', function () {
-        return view('auth.verify-email');
-    })->middleware('auth')->name('verification.notice');
-
-    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-        $request->fulfill();
-        
-        return redirect('/dashboard');
-    })->middleware(['auth', 'signed'])->name('verification.verify');
-
-    Route::post('/email/verification-notification', function (Request $request) {
-        $request->user()->sendEmailVerificationNotification();
-
-        return back()->with('message', 'Verification link sent!');
-    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
 });
 
 // ========= Authentication Routes (Laravel Breeze, Jetstream, etc.) =========
