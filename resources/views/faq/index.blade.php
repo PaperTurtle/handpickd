5f4d3bf655ca83aaf427254a0c6548a7d9714e5b
Tobias Neubert <tneubert36@gmail.com>
2023-11-28 20:09:37 +0100

    Copy-Paste question-answer 'template' to all categories

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -13,7 +13,7 @@
                 Artisans</button>
         </div>
         <ul>
-            <div x-show="openGeneral" x-data="{openGeneralQuestionOne: false, openGeneralQuestionTwo: false, openGeneralQuestionThree: false}">
+            <li x-show="openGeneral" x-data="{openGeneralQuestionOne: false, openGeneralQuestionTwo: false, openGeneralQuestionThree: false}">
                 <dl>
                     <!-- Question One -->
                     <div>
@@ -63,9 +63,127 @@
                         </dd>
                     </div>
                 </dl>
-            </div>
-            <dl x-show="openForCustomers"></dl>
-            <dl x-show="openForArtisans"></dl>
+            </li>
+            <!-- Customers -->
+            <li x-show="openForCustomers" x-data="{openCustomersQuestionOne: false, openCustomersQuestionTwo: false, openCustomersQuestionThree: false}">
+                <dl>
+                    <!-- Question One -->
+                    <div>
+                        <dt>
+                            <button @click="openCustomersQuestionOne = !openCustomersQuestionOne">
+                                <!-- Question -->
+                                <div>
+                                    question 1
+                                </div>
+                            </button>
+                        </dt>
+                        <dd x-show="openCustomersQuestionOne">
+                            <!-- Answer -->
+                            <p>answer 1</p>
+                        </dd>
+                    </div>
+
+                    <!-- Question two -->
+                    <div>
+                        <dt>
+                            <button @click="openCustomersQuestionTwo = !openCustomersQuestionTwo">
+                                <!-- Question -->
+                                <div>
+                                    question 2
+                                </div>
+                            </button>
+                        </dt>
+                        <dd x-show="openCustomersQuestionTwo">
+                            <!-- Answer -->
+                            <p>answer 2</p>
+                        </dd>
+                    </div>
+
+                    <!-- Question three -->
+                    <div>
+                        <dt>
+                            <button @click="openCustomersQuestionThree = !openCustomersQuestionThree">
+                                <!-- Question -->
+                                <div>
+                                    question 3
+                                </div>
+                            </button>
+                        </dt>
+                        <dd x-show="openCustomersQuestionThree">
+                            <!-- Answer -->
+                            <p>answer 3</p>
+                        </dd>
+                    </div>
+                </dl>
+            </li>
+            <!-- Artisans -->
+            <li x-show="openForArtisans" x-data="{openArtisansQuestionOne: false, openArtisansQuestionTwo: false, openArtisansQuestionThree: false, openArtisansQuestionFour: false}">
+                <dl>
+                    <!-- Question One -->
+                    <div>
+                        <dt>
+                            <button @click="openArtisansQuestionOne = !openArtisansQuestionOne">
+                                <!-- Question -->
+                                <div>
+                                    question 1
+                                </div>
+                            </button>
+                        </dt>
+                        <dd x-show="openArtisansQuestionOne">
+                            <!-- Answer -->
+                            <p>answer 1</p>
+                        </dd>
+                    </div>
+
+                    <!-- Question two -->
+                    <div>
+                        <dt>
+                            <button @click="openArtisansQuestionTwo = !openArtisansQuestionTwo">
+                                <!-- Question -->
+                                <div>
+                                    question 2
+                                </div>
+                            </button>
+                        </dt>
+                        <dd x-show="openArtisansQuestionTwo">
+                            <!-- Answer -->
+                            <p>answer 2</p>
+                        </dd>
+                    </div>
+
+                    <!-- Question three -->
+                    <div>
+                        <dt>
+                            <button @click="openArtisansQuestionThree = !openArtisansQuestionThree">
+                                <!-- Question -->
+                                <div>
+                                    question 3
+                                </div>
+                            </button>
+                        </dt>
+                        <dd x-show="openArtisansQuestionThree">
+                            <!-- Answer -->
+                            <p>answer 3</p>
+                        </dd>
+                    </div>
+                    
+                    <!-- Question four -->
+                    <div>
+                        <dt>
+                            <button @click="openArtisansQuestionFour = !openArtisansQuestionFour">
+                                <!-- Question -->
+                                <div>
+                                    question 4
+                                </div>
+                            </button>
+                        </dt>
+                        <dd x-show="openArtisansQuestionFour">
+                            <!-- Answer -->
+                            <p>answer 4</p>
+                        </dd>
+                    </div>
+                </dl>
+            </li>
         </ul>
     </div>
 </x-app-layout>
