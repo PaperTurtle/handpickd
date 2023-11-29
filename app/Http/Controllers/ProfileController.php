c831361aef77af687b030ea3f9bffe02a27bdc78
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-29 15:43:13 +0100

    Add show route and refactor others to use id

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,17 +17,29 @@ use Illuminate\View\View;
 class ProfileController extends Controller
 {
     /**
+     * Display the user's profile.
+     * This method returns the view for the user's profile with the user data.
+     * 
+     * @param int $userID The ID of the user whose profile is being displayed.
+     * @return View Returns the view for the user's profile with the user data.
+     */
+    public function show(int $userID): View
+    {
+        $user = User::findOrFail($userID);
+        return view('profile.show', ['user' => $user]);
+    }
+
+    /**
      * Display the user's profile form for editing.
      * This method returns the view for the user to edit their profile information.
      *
      * @param Request $request The current request instance.
      * @return View Returns the view for editing the user's profile with the user data.
      */
-    public function edit(Request $request): View
+    public function edit(int $userID): View
     {
-        return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
+        $user = User::findOrFail($userID);
+        return view('profile.edit', ['user' => $user]);
     }
 
     /**
@@ -36,19 +49,21 @@ class ProfileController extends Controller
      * It returns a redirect response to the profile edit page with a status message.
      *
      * @param ProfileUpdateRequest $request The request instance containing validated profile data.
+     * @param int $userID The ID of the user whose profile is being updated.
      * @return RedirectResponse Returns a redirect response to the profile edit page with a status message.
      */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
+    public function update(ProfileUpdateRequest $request, int $userID): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $user = User::findOrFail($userID);
+        $user->fill($request->validated());
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $user->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('profile.edit', $userID)->with('status', 'Profile updated successfully.');
     }
 
     /**
@@ -57,15 +72,16 @@ class ProfileController extends Controller
      * It logs out the user, invalidates the session, and redirects to the home page.
      *
      * @param Request $request The current request instance.
+     * @param int $userID The ID of the user whose account is being deleted.
      * @return RedirectResponse Returns a redirect response to the home page after account deletion. 
      */
-    public function destroy(Request $request): RedirectResponse
+    public function destroy(Request $request, int $userID): RedirectResponse
     {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
+        $user = User::findOrFail($userID);
 
-        $user = $request->user();
+        $request->validateWithBag('userDeletion', [
+            'password' => ['required', 'current_password:web'],
+        ]);
 
         Auth::logout();
 
