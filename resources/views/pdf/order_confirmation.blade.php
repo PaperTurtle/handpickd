437d0128d373c71e3b9597d64041acaa39417f57
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-11-30 21:57:25 +0100

    Add Styling, Still WIP

--- resources/views/pdf/order_confirmation.blade.php
+++ resources/views/pdf/order_confirmation.blade.php
@@ -4,59 +4,49 @@
 <head>
     <meta charset="UTF-8">
     <title>Order Details</title>
-    <style>
-        body {
-            font-family: 'DejaVu Sans', sans-serif;
-        }
-
-        .order-details {
-            width: 100%;
-        }
-
-        .product-item {
-            border-bottom: 1px solid #ddd;
-            padding: 8px;
-            display: flex;
-        }
-
-        .product-info {
-            flex-grow: 1;
-            margin-left: 16px;
-        }
-
-        .product-image {
-            width: 100px;
-            height: 100px;
-        }
-
-        .total {
-            text-align: right;
-        }
-    </style>
 </head>
 
-<body>
-    <div class="order-details">
+<body style="font-family: 'DejaVu Sans', sans-serif;">
+    <div style="width: 100%;">
         <h1>Thanks for ordering</h1>
         <p>We appreciate your order, the artisan is currently processing it.</p>
 
-        @php $orderPayment = 0; @endphp
+        @php
+            $orderPayment = 0;
+            $firstTransaction = reset($transactionDetails);
+            $buyer = $firstTransaction->transactionBuyer ?? null;
+        @endphp
+
+        @if ($buyer)
+            <div
+                style="margin-top: 20px; padding: 10px; background-color: #f8f8f8; border: 1px solid #eaeaea; border-radius: 8px;">
+                <h2 style="margin-top: 0; color: #333; font-size: 18px;">Buyer Information:</h2>
+                <p style="margin: 5px 0; font-size: 14px; color: #555;">Name: {{ $buyer->first_name }}
+                    {{ $buyer->last_name }}</p>
+                <p style="margin: 5px 0; font-size: 14px; color: #555;">Email: {{ $buyer->email }}</p>
+                <p style="margin: 5px 0; font-size: 14px; color: #555;">Phone: {{ $buyer->phone_number }}</p>
+                <p style="margin: 5px 0; font-size: 14px; color: #555;">Address: {{ $buyer->address }},
+                    {{ $buyer->city }}, {{ $buyer->state_province }} {{ $buyer->postal_code }}, {{ $buyer->country }}
+                </p>
+            </div>
+        @endif
+
         @foreach ($transactionDetails as $transaction)
             @php
                 $orderPayment += $transaction->total_price;
             @endphp
-            <div class="product-item">
+            <div style="border-bottom: 1px solid #ddd; padding: 8px; display: flex;">
                 <img src="{{ storage_path('app/public/' . $transaction->product->images->first()->thumbnail_image_path) }}"
-                    alt="Product Image" class="product-image">
-                <div class="product-info">
+                    alt="Product Image" style="width: 100px; height: 100px;">
+                <div style="flex-grow: 1; margin-left: 16px;">
                     <h3>{{ $transaction->product->name }}</h3>
                     <p>{{ $transaction->quantity }}x</p>
                 </div>
-                <p class="product-price">{{ number_format($transaction->total_price, 2) }} €</p>
+                <p style="flex-shrink: 0;">{{ number_format($transaction->total_price, 2) }} €</p>
             </div>
         @endforeach
 
-        <div class="total">
+        <div style="text-align: right;">
             <strong>Total: {{ number_format($orderPayment, 2) }} €</strong>
         </div>
     </div>
