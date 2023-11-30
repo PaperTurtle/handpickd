17206462ecdcbc586324cd9cf58f69fc07cb7afd
Tobias Neubert <tneubert36@gmail.com>
2023-11-30 16:35:35 +0100

    WIP: Add first draft of layout

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,5 +1,19 @@
 <x-app-layout>
-    @php
-        echo $user->transactions;
-    @endphp
+    <!-- Personal Information -->
+    <div></div>
+    <section>
+        <div>{{$user->name}}</div>
+    </section>
+    <!-- Your Products -->
+    <section>
+
+    </section>
+    <!-- Your Orders -->
+    <section>
+
+    </section>
+    <!-- Your Reviews -->
+    <section>
+
+    </section>
 </x-app-layout>
