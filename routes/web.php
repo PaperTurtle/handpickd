a70f4f80f1b35bc458468f9dfe5d0197f08113a3
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-01 09:12:12 +0100

    Remove whitespace

--- routes/web.php
+++ routes/web.php
@@ -81,13 +81,11 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
         $request->fulfill();
-     
         return redirect('/dashboard');
     })->middleware(['auth', 'signed'])->name('verification.verify');
 
     Route::post('/email/verification-notification', function (Request $request) {
         $request->user()->sendEmailVerificationNotification();
-     
         return back()->with('message', 'Verification link sent!');
     })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 });
