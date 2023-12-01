f4b01bf0788aed3621b60dcb286f27dfbe23baaf
Loran Heinzel <loran.heinzel@gmx.de>
2023-12-01 09:49:50 +0100

    Theme colors hinzugefügt

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -31,7 +31,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="bg-primary hover:bg-accent">
                 {{ __('Reset Password') }}
             </x-primary-button>
         </div>
