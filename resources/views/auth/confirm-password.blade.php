2cc56676cd560ecdd377937774d446a85624dfff
Loran Heinzel <loran.heinzel@gmx.de>
2023-12-01 09:49:46 +0100

    Theme colors hinzugefügt

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -19,7 +19,7 @@
         </div>
 
         <div class="flex justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="bg-primary hover:bg-accent">
                 {{ __('Confirm') }}
             </x-primary-button>
         </div>
