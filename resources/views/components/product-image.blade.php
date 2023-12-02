463afb490f36a43193380ea7627d5c264829db7d
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-02 18:11:14 +0100

    Delete component

--- resources/views/components/product-image.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg">
-    @foreach ($product->images as $image)
-        <img src="{{ Storage::url($image->show_image_path) }}" alt="{{ $image->alt_text }}"
-            class="h-full w-full object-cover object-center" loading="lazy">
-    @endforeach
-</div>
