533ba36b6dbb3c8f1a0338fef2040c4676ca82d3
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-02 14:11:36 +0100

    Add Content to Partners

--- resources/views/partners.blade.php
+++ resources/views/partners.blade.php
@@ -1,3 +1,43 @@
 <x-app-layout>
-
+    <div class="relative isolate px-6 lg:px-8">
+        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true">
+            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+            </div>
+        </div>
+        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div class="text-center">
+                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl font-heading">Our Partners</h1>
+                <p class="mt-6 text-lg leading-8 text-gray-600">We are proud to collaborate with a diverse group of
+                    partners who have made this project possible. Their support and contributions have been instrumental
+                    in bringing our vision to life.</p>
+                <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                        <div
+                            class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+                            <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                src="https://cdn.join.com/5de77942f3f7c50001e14c25/assecor-gmb-h-logo-xl.jpg"
+                                alt="Assecor GmbH" width="158" height="48">
+                            <img class="col-span-2 max-h-16 w-full object-contain lg:col-span-1"
+                                src="https://content.u-blox.com/sites/default/files/2023-03/R3%20Solutions%20Logo2.png"
+                                alt="R3 Solutions" width="158" height="48">
+                            <img class="col-span-2 max-h-16 w-full object-contain lg:col-span-1"
+                                src="https://frachtwerk.de/wp-content/uploads/2020/06/frachtwerk_320px.png" alt="Frachtwerk GmbH"
+                                width="158" height="48">
+                            <img class="col-span-2 max-h-16 w-full object-contain sm:col-start-2 lg:col-span-1"
+                                src="https://inside.iu-fernstudium.de/wp-content/uploads/2021/03/iu_Logo_D_black_RGB_horizontal.png"
+                                alt="IU" width="158" height="48">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true">
+            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+            </div>
+        </div>
+    </div>
 </x-app-layout>
