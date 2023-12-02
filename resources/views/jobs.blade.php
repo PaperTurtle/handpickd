e9779bf7a769d1afdd44a0a1d0d13df0221769a7
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-02 12:34:17 +0100

    Add Content to Jobs

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,3 +1,52 @@
 <x-app-layout>
-
+    <div class="bg-background py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
+                <div class="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
+                    <div>
+                        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
+                        <p class="mt-4 leading-7 text-gray-600">We're always interested in hearing from you!</p>
+                    </div>
+                    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+                        <div class="rounded-2xl bg-gray-50 p-10">
+                            <h3 class="text-base font-semibold leading-7 text-gray-900">Join our team</h3>
+                            <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                <div>
+                                    <dt class="sr-only">Email</dt>
+                                    <dd><a class="font-semibold text-indigo-600"
+                                            href="mailto:handpickd.shop@gmail.com">handpickd.shop@gmail.com</a></dd>
+                                </div>
+                            </dl>
+                        </div>
+                        <div class="rounded-2xl bg-gray-50 p-10">
+                            <h3 class="text-base font-semibold leading-7 text-gray-900">Say hello</h3>
+                            <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                <div>
+                                    <dt class="sr-only">Email</dt>
+                                    <dd><a class="font-semibold text-indigo-600"
+                                            href="mailto:handpickd.shop@gmail.com">handpickd.shop@gmail.com</a></dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-1 gap-x-8 gap-y-10 pt-16 lg:grid-cols-3">
+                    <div>
+                        <h2 class="text-3xl font-bold tracking-tight text-gray-900">Locations</h2>
+                        <p class="mt-4 leading-7 text-gray-600">We work from home, but we are mostly stationed in
+                            Germany</p>
+                    </div>
+                    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+                        <div class="rounded-2xl bg-gray-50 p-10">
+                            <h3 class="text-base font-semibold leading-7 text-gray-900">Berlin</h3>
+                            <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+                                <p>Frankfurter Allee 73a</p>
+                                <p>10247 Berlin</p>
+                            </address>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
