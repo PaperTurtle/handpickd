9361bcff87cdfe800e09d9deee7fc80818371380
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-02 19:51:50 +0100

    Update tests to work with middleware

--- tests/Feature/Controllers/DashboardControllerTest.php
+++ tests/Feature/Controllers/DashboardControllerTest.php
@@ -7,25 +7,31 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-test('markAsSent successfully marks a transaction as sent', function () {
-    $artisan = User::factory()->create();
-    $product = Product::factory()->create(['artisan_id' => $artisan->id]);
+test('markAsSent successfully marks a transaction as sent by owner artisan', function () {
+    $ownerArtisan = User::factory()->create(['isArtisan' => true]);
+    $product = Product::factory()->create(['artisan_id' => $ownerArtisan->id]);
     $transaction = Transaction::factory()->create(['product_id' => $product->id]);
 
-    $response = $this->actingAs($artisan)->patch(route('dashboard.markAsSent', ['transaction' => $transaction->id]));
+    $response = $this->actingAs($ownerArtisan)->patch(route('dashboard.markAsSent', ['transaction' => $transaction->id]));
 
     $response->assertStatus(200);
     $response->assertJson(['message' => 'Product marked as sent']);
 });
 
-test('markAsSent returns unauthorized for non-owner user', function () {
-    $artisan = User::factory()->create();
-    $otherUser = User::factory()->create();
-    $product = Product::factory()->create(['artisan_id' => $artisan->id]);
+test('markAsSent returns unauthorized for non-owner user or artisan', function () {
+    $ownerArtisan = User::factory()->create(['isArtisan' => true]);
+    $otherUser = User::factory()->create(['isArtisan' => false]);
+    $otherArtisan = User::factory()->create(['isArtisan' => true]);
+    $product = Product::factory()->create(['artisan_id' => $ownerArtisan->id]);
     $transaction = Transaction::factory()->create(['product_id' => $product->id]);
 
-    $response = $this->actingAs($otherUser)->patch(route('dashboard.markAsSent', ['transaction' => $transaction->id]));
+    // Non-owner user attempt
+    $responseUser = $this->actingAs($otherUser)->patch(route('dashboard.markAsSent', ['transaction' => $transaction->id]));
+    $responseUser->assertStatus(403);
+    $responseUser->assertJson(['message' => 'Unauthorized action']);
 
-    $response->assertStatus(403);
-    $response->assertJson(['message' => 'Unauthorized action']);
+    // Non-owner artisan attempt
+    $responseArtisan = $this->actingAs($otherArtisan)->patch(route('dashboard.markAsSent', ['transaction' => $transaction->id]));
+    $responseArtisan->assertStatus(403);
+    $responseArtisan->assertJson(['message' => 'Unauthorized action']);
 });
