04dfe0cd6f6af36166f893d2fb7e828d0b3f7e4b
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-03 16:31:01 +0100

    Change max 4

--- resources/views/components/image-upload.blade.php
+++ resources/views/components/image-upload.blade.php
@@ -1,6 +1,23 @@
-<div>
+<div x-data="{
+    images: [],
+    handleFileChange(event) {
+        this.images = [];
+        const files = Array.from(event.target.files).slice(0, 4); // Take only the first four files
+
+        files.forEach(file => {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                this.images.push(e.target.result);
+            };
+            reader.onerror = (e) => {
+                console.error('FileReader error: ', e);
+            };
+            reader.readAsDataURL(file);
+        });
+    }
+}">
     <label for="images" class="block text-sm font-medium leading-6 text-gray-900">Add More Images (up to
-        3):</label>
+        4):</label>
     <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 w-1/3 bg-white">
         <div class="text-center">
             <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
@@ -8,22 +25,16 @@
                     d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
                     clip-rule="evenodd" />
             </svg>
-            <div class="mt-4 flex text-sm leading-6 text-gray-600">
+            <div class="mt-4 flex justify-center text-sm leading-6 text-gray-600">
                 <label for="images"
                     class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                     <span>Upload files</span>
-                    <input type="file" id="images" name="images[]" type="file" multiple class="sr-only"
-                        accept="image/png, image/jpeg, image/jpg, image/gif, image/svg+xml"
-                        @change="images = []; Array.from($event.target.files).forEach(file => {
-                        let reader = new FileReader();
-                        reader.onload = (e) => { images.push(e.target.result); };
-                        reader.onerror = (e) => { console.error('FileReader error: ', e); };
-                        reader.readAsDataURL(file);
-                    })">
+                    <input type="file" id="images" name="images[]" class="sr-only"
+                        accept="image/png, image/jpeg, image/jpg, image/gif, image/svg+xml" @change="handleFileChange"
+                        multiple>
                 </label>
-                <p class="pl-1">or drag and drop</p>
             </div>
-            <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+            <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF and SVG</p>
         </div>
     </div>
     @error('images')
@@ -33,10 +44,8 @@
         <div class="text-sm text-red-600">{{ $message }}</div>
     @enderror
     <div id="image-preview-container" x-show="images.length > 0" class="mt-4 flex space-x-4">
-        <div id="image-preview-container" x-show="images.length > 0">
-            <template x-for="image in images" :key="image">
-                <img :src="image" style="height: 100px; margin-right: 10px;" alt="Image preview">
-            </template>
-        </div>
+        <template x-for="image in images" :key="image">
+            <img :src="image" class="h-[15.928em] w-[15.928em] rounded-md object-cover" alt="Image preview">
+        </template>
     </div>
 </div>
