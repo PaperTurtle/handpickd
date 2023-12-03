21f8927a63a90fcc36a25816f99c338d7bdf8882
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-03 18:27:49 +0100

    Update Policy

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -71,7 +71,7 @@ class ProductPolicy
      */
     public function delete(User $user, Product $product): bool
     {
-        return true;
+        return $user->id === $product->artisan_id;
     }
 
     /**
