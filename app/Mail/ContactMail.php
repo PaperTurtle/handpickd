b7b6404e1b6ea0b732606ebc6378cab2a9c4b757
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-03 13:47:45 +0100

    Add Mailable for Email

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * ContactMail is a mailable class responsible for constructing and sending contact form emails.
+ * It contains the data provided in the contact form and sends it to a predefined recipient.
+ */
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var array The data received from the contact form.
+     */
+    public $data;
+
+    /**
+     * Create a new message instance.
+     * Initializes the mail with data provided from the contact form.
+     *
+     * @param array $data Data passed to the email view.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     * Defines the subject of the email.
+     *
+     * @return Envelope Returns an Envelope instance with the subject of the email.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Contact Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     * Determines the view and data to be used in the email content.
+     *
+     * @return Content Returns a Content instance specifying the view and data for the email.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.send_contact',
+            with: [
+                'data' => $this->data,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment> Returns an array of attachments.
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
