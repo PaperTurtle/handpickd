edbbfa07a7fd8c06eb032fa112d9b5a35d0dbe9e
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-03 16:01:01 +0100

    Create error notfication

---
+++ resources/views/components/error-notification.blade.php
@@ -0,0 +1,27 @@
+<div aria-live="assertive" class="pointer-events-none fixed inset-0 flex px-4 py-6 items-start z-50">
+    <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
+            x-transition:enter="transform ease-out duration-300 transition"
+            x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
+            x-transition:leave="transition ease-in duration-100" x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+            class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div class="p-4">
+                <div class="flex items-start">
+                    <div class="flex-shrink-0">
+                        <svg class="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                        </svg>
+                    </div>
+                    <div class="ml-3 w-0 flex-1 pt-0.5">
+                        <h3 class="text-lg font-medium text-text">Error!</h3>
+                        <p class="mt-1 text-sm text-gray-500">{{ $slot }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
