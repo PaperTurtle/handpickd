b476fa4cdde682c5ab809aea9d75dec7deacc4b6
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-04 20:10:17 +0100

    Add components

---
+++ resources/views/components/profile-image.blade.php
@@ -0,0 +1,3 @@
+<img src="{{ Storage::url($product->artisan->profile->profile_picture) }}" alt="{{ $product->artisan->name }}"
+    class="w-8 h-8 rounded-full inline-flex">
+</img>
---
+++ resources/views/components/profile-initials.blade.php
@@ -0,0 +1,4 @@
+<span class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-gray-500">
+    <span class="text-md font-medium leading-none text-white">
+        {{ getUserInitials($product->artisan->name) }}</span>
+</span>
