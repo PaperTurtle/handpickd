77b450b8e79871a4bf2971d4bd6683f3e7b50904
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-04 10:29:49 +0100

    Update spinner

--- resources/views/checkout/process.blade.php
+++ resources/views/checkout/process.blade.php
@@ -8,7 +8,8 @@
             </a>
             <h1 class="text-3xl sm:text-4xl tracking-tight font-bold font-heading">Checkout</h1>
         </label>
-        <form class="pt-4" action="{{ route('checkout.process') }}" method="POST" x-cloak x-show="cartItems.length > 0">
+        <form class="pt-4" action="{{ route('checkout.process') }}" @submit="handleSubmit" method="POST" x-cloak
+            x-show="cartItems.length > 0">
             @csrf
             <div class="lg:grid grid-cols-12 lg:gap-x-12 xl:gap-x-16">
                 <section class="col-span-7 text-text lg:grid mt-10 pt-4 w-full border-t border-gray-200">
@@ -18,14 +19,14 @@
                         <div class="mt-4">
                             <div class="grid">
                                 <label>Email address</label>
-                                <input type="email" name="email" value={{ Auth::user()->email }}
+                                <input type="email" name="email" value={{ Auth::user()->email }} required
                                     class="h-8 text-sm rounded-md font-semibold shadow-sm  
                                     ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                     focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
                             </div>
                             <div class="grid mt-4">
                                 <label class="">Phone number</label>
-                                <input type="text" name="phone_number"
+                                <input type="text" name="phone_number" required
                                     class="h-8 text-sm rounded-md font-semibold shadow-sm  
                                     ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                     focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
@@ -44,35 +45,35 @@
                         <div class="flex mt-4 justify-between">
                             <div class="grid flex-grow">
                                 <label class="text-md">First name</label>
-                                <input type="text" name="first_name" value="{{ $first_name }}"
+                                <input type="text" name="first_name" value="{{ $first_name }}" required
                                     class="h-8 text-sm rounded-md font-semibold shadow-sm  
                                     ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                     focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
                             </div>
                             <div class="grid ml-4 flex-grow">
                                 <label class="text-md">Last name</label>
-                                <input type="text" name="last_name" value="{{ $last_name }}"
+                                <input type="text" name="last_name" value="{{ $last_name }}" required
                                     class="h-8 text-sm rounded-md font-semibold shadow-sm  
                                     ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                     focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
                             </div>
                         </div>
                         <label class="text-md mt-4">Address</label>
-                        <input type="text" name="address"
+                        <input type="text" name="address" required
                             class="w-full h-8 text-sm rounded-md font-semibold shadow-sm  
                                 ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                 focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
                         <div class="flex mt-4 justify-between">
                             <div class="grid flex-grow">
                                 <label class="text-md">City</label>
-                                <input type="text" name="city"
+                                <input type="text" name="city" required
                                     class="h-8 text-sm rounded-md font-semibold shadow-sm  
                                         ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                         focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
                             </div>
                             <div class="grid ml-4 flex-grow">
                                 <label class="text-md">Country</label>
-                                <input type="text" name="country"
+                                <input type="text" name="country" required
                                     class="h-8 text-sm rounded-md font-semibold shadow-sm  
                                         ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                         focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
@@ -88,7 +89,7 @@
                             </div>
                             <div class="grid ml-4 flex-grow">
                                 <label class="text-md">Postal code</label>
-                                <input type="text" name="postal_code"
+                                <input type="text" name="postal_code" required
                                     class="h-8 text-sm rounded-md font-semibold shadow-sm  
                                         ring-inset ring-gray-300 border-gray-300 hover:bg-gray-50
                                         focus:ring-2 focus:ring-accent focus:border-accent transition-all delay-[20ms]"></input>
@@ -173,9 +174,20 @@
                     </dl>
                     <!-- checkout button -->
                     <div class="pt-4">
-                        <button type="submit"
-                            class="bg-primary text-base hover:bg-accent text-white font-bold py-2 px-4 rounded-lg min-w-full transition-all delay-[20ms]">
-                            Confirm Order
+                        <button type="submit" :disabled="loading"
+                            class="flex justify-center gap-2 bg-primary text-base hover:bg-accent text-white font-bold py-2 px-4 rounded-lg min-w-full transition-all delay-[20ms]">
+                            Confirm Order <div x-show="loading" class="flex justify-center items-center">
+                                <svg aria-hidden="true"
+                                    class="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-white"
+                                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                        fill="currentColor" />
+                                    <path
+                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                        fill="currentFill" />
+                                </svg>
+                            </div>
                         </button>
                     </div>
                 </section>
@@ -185,8 +197,10 @@
     <script>
         function cart() {
             return {
+                loading: false,
                 cartItems: @json($cartItems),
                 deliveryMethod: 'Standard',
+
                 calculateSubtotalPrice() {
                     let subTotalPrice = 0;
                     this.cartItems.forEach(cartItem => {
