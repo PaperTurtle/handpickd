0c8f96642a64229b99430e2f3f0b968c17ca91e4
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-04 15:53:20 +0100

    Change Stroke width

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -50,7 +50,7 @@
                                         class="inline-flex items-center text-green-500 font-bold">
                                         <span>Order Completed</span>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="ml-2 w-6 h-6">
+                                            stroke-width="2.5" stroke="currentColor" class="ml-2 w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M4.5 12.75l6 6 9-13.5" />
                                         </svg>
