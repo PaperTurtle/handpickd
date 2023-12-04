c727cf52b5f52f303db8f856e10902e19aa720c3
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-04 21:01:44 +0100

    Change color and add animation

--- resources/views/components/profile-initials.blade.php
+++ resources/views/components/profile-initials.blade.php
@@ -1,4 +1,5 @@
-<span class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-gray-500">
+<span
+    class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-primary hover:bg-accent transition-all delay-[5ms]">
     <span class="text-md font-medium leading-none text-white">
         {{ getUserInitials($product->artisan->name) }}</span>
 </span>
