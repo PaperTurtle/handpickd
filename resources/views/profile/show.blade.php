5816ad3f5b767acae35e5fcec3381944961b76e3
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-04 17:02:07 +0100

    Add images and change section

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,32 +1,33 @@
 <x-app-layout>
     <div class="container mx-auto px-4 pb-24 pt-16 sm:px-6 max-w-4xl lg:px-8 text-text text-sm">
-        <h2 class="text-3xl font-bold">Profile</h2>
+        <h2 class="text-4xl font-bold font-heading">Profile</h2>
         <!-- Personal Information -->
-        <h2 class="text-2xl mt-10">Personal information</h2>
+        <h2 class="text-2xl mt-10 font-heading">Personal information</h2>
         <section class="mt-6 border-t border-b border-gray-300 pt-8 pb-6 h-fit">
             <div class="md:flex">
                 <div>
-                    <img src="{{Storage::url($user->profile->profile_picture)}}" alt="" class="w-60 h-60">
+                    <img src="{{ Storage::url($user->profile->profile_picture) }}" alt=""
+                        class="w-60 h-60 rounded-full">
                     </img>
                 </div>
                 <div class="md:ml-40">
                     <div>
                         <div class="mt-4 font-bold">Name:</div>
-                        <div class="mt-1"> {{$user->name}}</div>
+                        <div class="mt-1"> {{ $user->name }}</div>
                     </div>
                     <div>
                         <div class="mt-4 font-bold">Email:</div>
-                        <div class="mt-1">{{$user->email}}</div>
+                        <div class="mt-1">{{ $user->email }}</div>
                     </div>
                     <div>
 
                     </div>
                     <div class="mt-4 font-bold">Bio:</div>
-                    <div class="mt-1">{{$user->profile->bio}}</div>
+                    <div class="mt-1">{{ $user->profile->bio }}</div>
                 </div>
             </div>
             <div class="relative mt-16 mb-4">
-                <a href="{{ route('profile.edit', auth()->user()->id)}}"><button type="submit"
+                <a href="{{ route('profile.edit', auth()->user()->id) }}"><button type="submit"
                         class="absolute bottom-0 right-0 w-20 py-2 bg-accent hover:bg-primary rounded-md text-white">Edit</button>
                 </a>
 
@@ -34,83 +35,91 @@
         </section>
 
         @php
-        $hasProducts = sizeof($user->products) > 0;
-        $hasOrders = sizeof($user->transactions) > 0;
+            $hasProducts = sizeof($user->products) > 0;
+            $hasOrders = sizeof($user->transactions) > 0;
         @endphp
 
         @if ($user->isArtisan)
-        <!-- Your Products -->
-        <section class="border-b border-gray-300 mt-6 pb-8 h-fit">
-            <h3 class="text-2xl">Your Products</h3>
-            @if ($hasProducts)
-            <div class="grid md:grid-cols-2 sm:grid-cols-1 gap-4">
-                @foreach ($user->products as $product)
-                <div class="mt-4 mb-4 justify-between grid bg-light-grey rounded-md p-4">
-                    <div class="flex relative">
-                        <div class="mr-4">
-                            <img class="w-32 h-32" src=""></img>
-                        </div>
-                        <div class="relative">
-                            <div class="font-medium">
-                                <a href="`{{ route('products.show', '')}}`"
-                                    class="font-medium text-text hover:text-gray-400">{{$product->name}}</a>
+            <!-- Your Products -->
+            <section class="border-b border-gray-300 mt-6 pb-8">
+                <h3 class="text-3xl font-heading">Your Products</h3>
+                @if ($hasProducts)
+                    <div class="mt-6 grid grid-cols-1 gap-x-3 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-3">
+                        @foreach ($user->products as $product)
+                            <div class="group relative">
+                                <div
+                                    class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                    <img src="{{ Storage::url($product->images->first()->resized_image_path) }}"
+                                        alt="{{ $product->description }}"
+                                        class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                                </div>
+                                <div class="mt-4 flex justify-between">
+                                    <div>
+                                        <h3 class="text-sm text-gray-700">
+                                            <a href="{{ route('products.show', $product->id) }}">
+                                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                                {{ $product->name }}
+                                            </a>
+                                        </h3>
+                                        <p class="mt-1 text-sm text-gray-500">{{ $product->category->name }}</p>
+                                    </div>
+                                    <p class="text-sm font-medium text-gray-900">{{ $product->price }} €</p>
+                                </div>
+                                <div class="mt-2 flex justify-center gap-4">
+                                    <button>Delete</button>
+                                    <button>Edit</button>
+                                </div>
                             </div>
-                            <div class="mt-4">{{$product->price}} €</div>
-                            <div class="mt-8">{{$product->description}}</div>
-                        </div>
+                        @endforeach
                     </div>
-                    <button type="submit"
-                        class="mt-4 w-20 h-10 rounded-lg bg-accent hover:bg-primary p-1 text-white ml-auto mr-4">Edit</button>
-                </div>
-                @endforeach
-            </div>
-            @endif
-            @if (!$hasProducts)
-            <div class="mt-4">
-                You are registered as artisan, but you haven't uploaded any goods so far.
-            </div>
-            @endif
-        </section>
+                @endif
+                @if (!$hasProducts)
+                    <div class="mt-4">
+                        You are registered as artisan, but you haven't uploaded any goods so far.
+                    </div>
+                @endif
+            </section>
         @endif
 
         <!-- Your Orders -->
         <section class="border-b border-gray-300 pb-6 ">
-            <h3 class="text-2xl mb-4 mt-6">Your Orders</h3>
-            @if($hasOrders)
-            @php
-            $groupedTransactions = [];
-            foreach($user->transactions as $transaction){
-            $date = strval($transaction->created_at);
+            <h3 class="text-2xl mb-4 mt-6 font-heading">Your Orders</h3>
+            @if ($hasOrders)
+                @php
+                    $groupedTransactions = [];
+                    foreach ($user->transactions as $transaction) {
+                        $date = strval($transaction->created_at);
 
-            if (!isset($groupedTransactions[$date][0])){
-            $groupedTransactions[$date] = [];
-            }
-            array_push($groupedTransactions[$date], $transaction);
-            }
-            @endphp
-            @foreach ($groupedTransactions as $date => $transactions)
-            <div class="text-xl mt-6">Ordered on: {{$transaction->created_at->format('Y-m-d H:i')}}</div>
-            <div class="grid md:grid-cols-2 sm:grid-cols-1 gap-4 my-4 bg-light-grey rounded-md">
-                @foreach ($transactions as $transaction)
-                <div class="mt-4 mb-4 justify-between grid p-4">
-                    <div class="flex relative">
-                        <div class="mr-4">
-                            <img class="w-32 h-32" src=""></img>
-                        </div>
-                        <div class="relative">
-                            <div class="font-medium">{{$transaction->product->name}}</div>
-                            <div class="mt-4">Quantity: {{$transaction->quantity}} pcs.</div>
-                            <div class="mt-4">Price: {{$transaction->total_price}} €</div>
-                            <div class="mt-4">Status: {{$transaction->status}}</div>
-                        </div>
+                        if (!isset($groupedTransactions[$date][0])) {
+                            $groupedTransactions[$date] = [];
+                        }
+                        array_push($groupedTransactions[$date], $transaction);
+                    }
+                @endphp
+                @foreach ($groupedTransactions as $date => $transactions)
+                    <div class="text-xl mt-6">Ordered on: {{ $transaction->created_at->format('Y-m-d H:i') }}</div>
+                    <div class="grid md:grid-cols-2 sm:grid-cols-1 gap-4 my-4 bg-light-grey rounded-md">
+                        @foreach ($transactions as $transaction)
+                            <div class="mt-4 mb-4 justify-between grid p-4">
+                                <div class="flex relative">
+                                    <div class="mr-4">
+                                        <img class="w-32 h-32 rounded-md"
+                                            src="{{ Storage::url($transaction->product->images->first()->thumbnail_image_path) }}"></img>
+                                    </div>
+                                    <div class="relative">
+                                        <div class="font-medium">{{ $transaction->product->name }}</div>
+                                        <div class="mt-4">Quantity: {{ $transaction->quantity }} pcs.</div>
+                                        <div class="mt-4">Price: {{ $transaction->total_price }} €</div>
+                                        <div class="mt-4">Status: {{ $transaction->status }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                </div>
                 @endforeach
-            </div>
-            @endforeach
             @else
-            <div>You havn't ordered anything yet.</div>
-            <a href="{{route('products.index')}}">Visit the product page to order something.</a>
+                <div>You havn't ordered anything yet.</div>
+                <a href="{{ route('products.index') }}">Visit the product page to order something.</a>
             @endif
         </section>
     </div>
