48046b29c074086c1dbe4dd680acaea6b70ee6f9
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-05 08:20:07 +0100

    Update bio to no longer have max (VARCHAR => LONGTEXT in db)

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,7 +18,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'bio' => ['nullable', 'string', 'max:255'],
+            'bio' => ['nullable', 'string'],
             'profile_picture' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:10240'],
         ];
     }
