b78ec280ac291e07af5917013367c20e7f892b26
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-05 11:51:10 +0100

    Update contact with required

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -20,28 +20,28 @@
                     <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First
                         name</label>
                     <div class="mt-2.5">
-                        <input type="text" name="first-name" id="first-name" autocomplete="given-name"
+                        <input type="text" name="first-name" id="first-name" required
                             class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                 </div>
                 <div>
                     <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
                     <div class="mt-2.5">
-                        <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                        <input type="text" name="last-name" id="last-name" required
                             class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                 </div>
                 <div class="sm:col-span-2">
                     <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
                     <div class="mt-2.5">
-                        <input type="email" name="email" id="email" autocomplete="email"
+                        <input type="email" name="email" id="email" required
                             class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                 </div>
                 <div class="sm:col-span-2">
                     <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
                     <div class="mt-2.5">
-                        <textarea name="message" id="message" rows="4"
+                        <textarea name="message" id="message" rows="4" required
                             class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                     </div>
                 </div>
