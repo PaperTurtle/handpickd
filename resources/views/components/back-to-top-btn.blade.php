b1fd57ec8e1abbbcbff0c1843b0f53a447671769
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-06 07:56:34 +0100

    Add animation

--- resources/views/components/back-to-top-btn.blade.php
+++ resources/views/components/back-to-top-btn.blade.php
@@ -1,6 +1,6 @@
 <button id="back-to-top-btn">
-    <svg class="h-6 w-6 text-white ml-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg">
+    <svg class="h-6 w-6 text-white ml-3 hover:-translate-y-1 transition-all delay-[10ms] ease-out" fill="none"
+        stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
     </svg>
 </button>
