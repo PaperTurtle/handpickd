f9e997d8b30873dea7c5daede30503e13ef17eeb
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-06 16:31:36 +0100

    Reduce padding

--- resources/views/components/introduction.blade.php
+++ resources/views/components/introduction.blade.php
@@ -1,4 +1,4 @@
-<div class="py-24 text-center">
+<div class="py-12 text-center">
     <h1 class="text-4xl font-bold tracking-tight text-text font-heading">Discover Artisanal Wonders</h1>
     <p class="mx-auto mt-4 max-w-3xl text-base text-gray-500">Explore a curated selection of handcrafted
         treasures, each piece telling a unique story of skill and creativity, perfect for enhancing your
