45d7f1a0e51fb9a718b740fdd8d962384ce44514
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-07 18:31:17 +0100

    Add smooth animation

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -4,21 +4,21 @@
         <h2 class="text-2xl mt-10">Frequently asked questions</h2>
         <div class="flex flex-col gap-2 sm:flex-row justify-between mt-5">
             <label
-                class="bg-white border-2 border-grey-300 rounded-md px-5 py-3 mr-2 text-xl text-center font-semi-bold w-full cursor-pointer"
-                :class="{ 'border-accent': chosenTopic === 'general' }">
+                class="bg-white border-2 border-gray-300 rounded-md px-5 py-3 mr-2 text-xl text-center font-semibold w-full cursor-pointer transition-all delay-[10ms] ease-in-out"
+                :class="{ 'border-[#538a38]': chosenTopic === 'general', 'hover:border-gray-400': chosenTopic !== 'general' }">
                 <input type="radio" name="topic" value="general" class="sr-only" x-model="chosenTopic">General</input>
             </label>
 
             <label
-                class="bg-white border-2 border-grey-300 rounded-md px-5 py-3 mr-2 text-xl text-center font-semi-bold w-full cursor-pointer"
-                :class="{ 'border-accent': chosenTopic === 'customers' }">
+                class="bg-white border-2 border-gray-300 rounded-md px-5 py-3 mr-2 text-xl text-center font-semibold w-full cursor-pointer transition-all delay-[10ms] ease-in-out"
+                :class="{ 'border-[#538a38]': chosenTopic === 'customers', 'hover:border-gray-400': chosenTopic !== 'customers' }">
                 <input type="radio" name="topic" value="customers" class="sr-only" x-model="chosenTopic">For our
                 Customers</input>
             </label>
 
             <label
-                class="bg-white border-2 border-grey-300 rounded-md px-5 py-3 text-xl text-center font-semi-bold w-full cursor-pointer"
-                :class="{ 'border-accent': chosenTopic === 'artisans' }">
+                class="bg-white border-2 border-gray-300 rounded-md px-5 py-3 text-xl text-center font-semibold w-full cursor-pointer transition-all delay-[10ms] ease-in-out"
+                :class="{ 'border-[#538a38]': chosenTopic === 'artisans', 'hover:border-gray-400': chosenTopic !== 'artisans' }">
                 <input type="radio" name="topic" value="artisans" class="sr-only" x-model="chosenTopic">For our
                 Artisans</input>
             </label>
