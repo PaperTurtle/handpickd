b012fbfd84e452e369284e11a41b8cf19cb11852
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-10 15:04:10 +0100

    Remove Import

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\UpdateCartRequest;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use App\Models\ShoppingCart;
 use App\Services\CartService;
 use App\Services\CheckoutService;
