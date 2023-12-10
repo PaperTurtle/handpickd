4ee1b8ee03f4f5300109ffe2a203e21a71ad3aa0
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-10 15:04:19 +0100

    Add global variable for Cart

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
+use App\Models\ShoppingCart;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            if (Auth::check()) {
+                $cartItemCount = ShoppingCart::where('user_id', Auth::id())
+                    ->distinct('product_id')
+                    ->count('product_id');
+            } else {
+                $cartItemCount = 0;
+            }
+            $view->with('cartItemCount', $cartItemCount);
+        });
     }
 }
