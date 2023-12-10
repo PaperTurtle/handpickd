7a8cedf4b59bca6ad7a019756b94c649572f8076
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-10 15:04:01 +0100

    Start Count for Shopping Cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,13 @@ import Alpine from "alpinejs";
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener("alpine:init", () => {
+	Alpine.store("cart", {
+		count: 0,
+
+		increment() {
+			this.count++;
+		},
+	});
+});
