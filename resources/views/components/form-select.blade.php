3fa9c42e04cd9e33571f0c4c2c740bb4cee4cdb8
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-12 16:51:11 +0100

    Fix Typo

--- resources/views/components/form-select.blade.php
+++ resources/views/components/form-select.blade.php
@@ -2,7 +2,7 @@
     <label class="block text-base font-medium leading-6 text-gray-900"
         for="{{ $name }}">{{ $label }}</label>
     <select
-        class="w-full mt-1 block rounded-md border-gray-300 shadow-sm ocus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50 delay-[10ms] transition-all ease-in-out"
+        class="w-full mt-1 block rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50 delay-[10ms] transition-all ease-in-out"
         name="{{ $name }}" id="{{ $name }}" required {{ $attributes }}>
         {{ $slot }}
     </select>
--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -2,7 +2,7 @@
     <label class="block text-base font-medium leading-6 text-gray-900"
         for="{{ $name }}">{{ $slot }}</label>
     <textarea
-        class="w-full mt-1 block rounded-md border-gray-300 shadow-sm ocus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50 delay-[10ms] transition-all ease-in-out"
+        class="w-full mt-1 block rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50 delay-[10ms] transition-all ease-in-out"
         id="{{ $name }}" name="{{ $name }}" {{ $attributes }}>{{ $value ?? '' }}</textarea>
     @error($name)
         <div class="alert alert-danger">{{ $message }}</div>
