bad1029c5c7c9555b7a7ab686ed1f0963ae4d9a5
Seweryn Czabanowski <seweryn_cz@outlook.com>
2023-12-20 11:27:55 +0100

    Add nowrap for FAQ/Support link

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active'])
 
 @php
-    $classes = $active ?? false ? 'inline-flex items-center py-2 px-2 md:px-4 rounded-full bg-background font-semibold transition-colors duration-300 text-sm lg:text-base' : 'inline-flex items-center py-2 px-4 rounded-full hover:bg-secondary hover:text-text transition-colors duration-300 text-sm lg:text-base text-background font-semibold';
+    $classes = $active ?? false ? 'inline-flex items-center py-2 px-2 md:px-4 rounded-full bg-background font-semibold transition-colors duration-300 text-sm lg:text-base whitespace-nowrap' : 'inline-flex items-center py-2 px-4 rounded-full hover:bg-secondary hover:text-text transition-colors duration-300 text-sm lg:text-base text-background font-semibold whitespace-nowrap';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
